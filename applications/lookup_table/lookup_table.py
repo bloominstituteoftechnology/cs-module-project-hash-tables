d9c107b3830e1cc1a9d2e5f5b33dbfce477defa9
danpatrickoneill <dan47573@gmail.com>
2020-05-30 17:58:38 -0400

    Lot of work, not much progress

--- applications/lookup_table/lookup_table.py
+++ applications/lookup_table/lookup_table.py
@@ -1,4 +1,4 @@
-# Your code here
+import random, math
 
 
 def slowfun_too_slow(x, y):
@@ -13,9 +13,45 @@ def slowfun(x, y):
     """
     Rewrite slowfun_too_slow() in here so that the program produces the same
     output, but completes quickly instead of taking ages to run.
-    """
-    # Your code here
+    """    
+    products = {}
+    factorials = {}
+    quotients = {}
+    remainders = {}
+    
+    def helper(a, b):
+        # products[(a, b)] = math.pow(a, b)
+        build_products(a, b)
+        # factorials[a, b] = math.factorial(products[a, b])
+        build_factorials(products[(a, b)])
+        quotients[a, b] = factorials[products[(a, b)]] // (a + b)
+        remainders[a, b] =  quotients[a, b] % 982451653
 
+    def build_products(a, b):
+        total = 1
+        for i in range(b + 1):
+            products[a, i] = total
+            total *= a
+
+    def build_factorials(num):
+        while num not in factorials:
+            if num + 1 in factorials:
+                factorials[num] = factorials[num + 1] / num + 1
+                if num:
+                    num -= 1
+            elif num - 1 in factorials:
+                factorials[num] = factorials[num - 1] * num
+            else:
+                factorials[num] = math.factorial(num)
+
+
+    if (x, y) not in products:
+        for i in range(1, x+1):
+            for j in range(1, y+1):
+                if (i, j) not in products:
+                    helper(i, j)
+
+    return remainders[(x,y)]
 
 
 # Do not modify below this line!
