381f90c52d23fae7108e90fd31fcd9970915caa9
Nicole white <you@example.com>
2020-06-01 19:21:33 -1000

    through dupes

--- applications/no_dups/no_dups.py
+++ applications/no_dups/no_dups.py
@@ -1,7 +1,19 @@
 def no_dups(s):
-    # Your code here
-
-
+    # Your code
+    #List to hold non duplicates 
+    lst_of_keys = []
+    if s == '':
+        # Return empty string if it is given
+        return ''
+    # Split string input into a list
+    l = s.split(' ')
+    # Turning array into dict as keys removes duplicates
+    a = dict.fromkeys(l)
+    for key in a.keys():
+        # Loops keys and appends them to List of keys
+        lst_of_keys.append(key)
+        #Join it all for the return with a space between ' ' in order to maintain space distance
+    return ' '.join(lst_of_keys)
 
 if __name__ == "__main__":
     print(no_dups(""))
--- applications/word_count/word_count.py
+++ applications/word_count/word_count.py
@@ -1,5 +1,11 @@
+cache = {}
 def word_count(s):
-    # Your code here
+    name= s.replace('"','').replace(':','').replace(';','').replace(',','').replace('.','').replace('-','').replace('+','').replace('=','').replace('/','').replace("|",'').replace('[','').replace(']','').replace('{','').replace('}','').replace('(','').replace(')','').replace('*','').replace('^','').replace('&','').replace('\\','')
+    if name == '':
+        return {}
+    split = name.lower().split()
+    words = {i:split.count(i) for i in split}
+    return words
 
 
 
@@ -8,3 +14,5 @@ if __name__ == "__main__":
     print(word_count("Hello"))
     print(word_count('Hello, my cat. And my cat doesn\'t say "hello" back.'))
     print(word_count('This is a test of the emergency broadcast network. This is only a test.'))
+
+  
