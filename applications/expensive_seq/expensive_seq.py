4a46388647f4137bd3627778a914cebe53d1d516
mefrem <maxim.efremov@gmail.com>
2020-06-03 13:32:52 -0400

    Expensive sequence assignment

--- applications/expensive_seq/expensive_seq.py
+++ applications/expensive_seq/expensive_seq.py
@@ -1,14 +1,33 @@
 # Your code here
-
-
-def expensive_seq(x, y, z):
+cache = {}
+def exps(x, y, z):
     # Your code here
 
+    key = (x, y, z)
+    if key in cache:
+        return cache[key]
+    else:
+        if x <= 0:
+            return y + z
+        if x >  0:
+            a = exps(x-1,y+1,z)
+            key_a = (x-1,y+1,z)
+            cache[key_a] = a
+
+            b = exps(x-2,y+2,z*2)
+            key_b = (x-2,y+2,z*2)
+            cache[key_b] = b
+
+            c = exps(x-3,y+3,z*3)
+            key_c = (x-3,y+3,z*3)
+            cache[key_c] = c
+
+            return a + b + c
 
 
 if __name__ == "__main__":
     for i in range(10):
-        x = expensive_seq(i*2, i*3, i*4)
+        x = exps(i*2, i*3, i*4)
         print(f"{i*2} {i*3} {i*4} = {x}")
 
-    print(expensive_seq(150, 400, 800))
+    print(exps(150, 400, 800))
