6bb971ac1c641a063a1add427b2e1e8d12ad9281
Chad Rutherford <chad_rutherford@outlook.com>
2020-06-04 16:58:06 -0400

    finished histo.py

--- applications/markov/markov.py
+++ applications/markov/markov.py
@@ -3,6 +3,8 @@ import random
 # Read in all the words in one go
 with open("input.txt") as f:
     words = f.read()
+    split_words = words.split()
+    
 
 # TODO: analyze which words can follow other words
 # Your code here
