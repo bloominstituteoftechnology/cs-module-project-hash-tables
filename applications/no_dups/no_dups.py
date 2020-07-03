d16cb39f243e24aae49ccc2d257058a784416d5f
Michael McGrath <maybemichael@icloud.com>
2020-07-03 04:15:51 -0700

    Finished No Dups

--- applications/no_dups/no_dups.py
+++ applications/no_dups/no_dups.py
@@ -1,6 +1,14 @@
 def no_dups(s):
     # Your code here
-
+    words = s.split()
+    word_count = {}
+    unique_words = ""
+    for word in words:
+        if word_count.__contains__(word) is False:
+            word_count[word] = 1
+            unique_words += f"{word} "
+        
+    return unique_words.rstrip()
 
 
 if __name__ == "__main__":
@@ -9,3 +17,5 @@ if __name__ == "__main__":
     print(no_dups("hello hello"))
     print(no_dups("cats dogs fish cats dogs"))
     print(no_dups("spam spam spam eggs spam sausage spam spam and spam"))
+    x = no_dups("hello")
+    print(f"This is X: {x}")
