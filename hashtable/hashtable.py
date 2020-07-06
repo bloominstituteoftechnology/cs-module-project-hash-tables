a3595f035ae9d2f2d5a01ba747270175d44fe793
Mike Sinnema <msinnema33@gmail.com>
2020-07-06 11:54:56 -0700

    updated hashtable for day 1 and day 2 homework

--- hashtable/hashtable.py
+++ hashtable/hashtable.py
@@ -1,3 +1,7 @@
+from linked_list import LinkedList
+import sys
+sys.path.append('../hashtable/linked_list')
+
 class HashTableEntry:
     """
     Linked List hash table key/value pair
@@ -7,6 +11,8 @@ class HashTableEntry:
         self.value = value
         self.next = None
 
+    def __repr__(self):
+        return f'HashTableEntry({repr(self.key)}, {repr(self.value)})'
 
 # Hash table can't have fewer than this many slots
 MIN_CAPACITY = 8
@@ -20,8 +26,10 @@ class HashTable:
     Implement this.
     """
 
-    def __init__(self, capacity):
-        # Your code here
+    def __init__(self, capacity = MIN_CAPACITY):
+        self.storage = [LinkedList()] * MIN_CAPACITY
+        self.count = 0
+        self.capacity = capacity
 
 
     def get_num_slots(self):
@@ -35,6 +43,7 @@ class HashTable:
         Implement this.
         """
         # Your code here
+        return self.capacity
 
 
     def get_load_factor(self):
@@ -44,6 +53,7 @@ class HashTable:
         Implement this.
         """
         # Your code here
+        return self.count / self.capacity
 
 
     def fnv1(self, key):
@@ -54,6 +64,16 @@ class HashTable:
         """
 
         # Your code here
+        FNV_prime = 1099511628211
+        offset_basis = 14695981039346656037
+        seed = 0
+
+        #hash function
+        hash = offset_basis + seed
+        for c in key:
+            hash = hash ^ ord(c)
+            hash = hash * FNV_prime
+        return hash
 
 
     def djb2(self, key):
@@ -63,6 +83,10 @@ class HashTable:
         Implement this, and/or FNV-1.
         """
         # Your code here
+        # hash = 5381
+        # for c in key:
+        #     hash = (( hash << 5) + hash) + ord(c)
+        # return hash & 0xFFFFFFFF
 
 
     def hash_index(self, key):
@@ -70,8 +94,8 @@ class HashTable:
         Take an arbitrary key and return a valid integer index
         between within the storage capacity of the hash table.
         """
-        #return self.fnv1(key) % self.capacity
-        return self.djb2(key) % self.capacity
+        return self.fnv1(key) % len(self.storage)
+        # return self.djb2(key) % self.capacity
 
     def put(self, key, value):
         """
@@ -82,6 +106,17 @@ class HashTable:
         Implement this.
         """
         # Your code here
+        slot = self.hash_index(key)
+        current = self.storage[slot].head
+
+        while current:
+            if current.key == key:
+                current.value = value
+            current = current.next
+
+        entry = HashTableEntry(key, value)
+        self.storage[slot].insert_at_head(entry)
+        self.count += 1
 
 
     def delete(self, key):
@@ -93,6 +128,8 @@ class HashTable:
         Implement this.
         """
         # Your code here
+        self.put(key, None)
+        self.count -= 1
 
 
     def get(self, key):
@@ -104,6 +141,14 @@ class HashTable:
         Implement this.
         """
         # Your code here
+        slot = self.hash_index(key)
+        current = self.storage[slot].head
+
+        while current:
+            if current.key == key:
+                return current.value
+            current = current.next
+        return None
 
 
     def resize(self, new_capacity):
@@ -115,7 +160,15 @@ class HashTable:
         """
         # Your code here
 
-
+        if self.get_load_factor() >= 0.7:
+            old_storage = self.storage
+            self.storage = [LinkedList()] * new_capacity
+            for item in old_storage:
+                current = item.head
+                while current:
+                    self.put(current.key, current.value)
+                    current = current.next
+            self.capacity = new_capacity
 
 if __name__ == "__main__":
     ht = HashTable(8)
