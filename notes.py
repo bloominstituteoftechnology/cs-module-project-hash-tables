5e473653470baadc361c50e34fb27b4c73851320
Brandon-Harris <brandonharris177@gmail.com>
2020-07-06 11:06:35 -0600

    added notes file with comments from this morning

---
+++ notes.py
@@ -0,0 +1,39 @@
+arr = [None]*8
+
+def my_hash(s):
+    e = s.encode()
+    total = 0
+    for c in e:
+        total += c
+
+    return total%len(arr)
+
+first_total = my_hash("Brandon")
+
+arr[first_total] = "Data"
+
+def get_length(input):
+    curr_hash = my_hash(input)
+    return arr[curr_hash]
+
+length_Brandon = get_length("Brandon")
+
+print(length_Brandon)
+
+# def my_hash(s):
+#     sb = s.encode()
+#     total = 0
+#     for b in sb:
+#         total += b
+#     return total%8
+# i = my_hash("parth")
+# hash_table = [None]*8
+# hash_table[i] = "8 months"
+# ##hash parth
+# ##retrieve value at that hash
+# def get_length_timeatlambda(e):
+#     curr_hash = my_hash(e)
+#     return hash_table[curr_hash]
+# length_parth = get_length_timeatlambda("parth")
+# print("Parth has worked at Lambda for " + length_parth)
+# employees = {}
