3cb2a1912bfac4a1d6afc70bef59e4554745d8a6
Hector <hectoremil21@hotmail.com>
2020-07-06 21:50:24 -0400

    Implement djb2()

--- hashtable/hashtable.py
+++ hashtable/hashtable.py
@@ -68,6 +68,10 @@ class HashTable:
         Implement this, and/or FNV-1.
         """
         # Your code here
+        hash = 5381
+        for val in key:
+            hash = ((hash << 5) + hash) + ord(val)
+        return hash
 
 
     def hash_index(self, key):
