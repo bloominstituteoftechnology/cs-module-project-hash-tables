203996aba3675ff6a82396708b77cfd18c17b854
Chris Hartig <chrishartig41@gmail.com>
2020-07-07 11:09:11 -0500

    Monday assignment

--- hashtable/hashtable.py
+++ hashtable/hashtable.py
@@ -16,12 +16,16 @@ class HashTable:
     """
     A hash table that with `capacity` buckets
     that accepts string keys
-
     Implement this.
     """
 
     def __init__(self, capacity):
-        # Your code here
+        if capacity < MIN_CAPACITY:
+            self.capacity = MIN_CAPACITY
+        else:
+            self.capacity = capacity
+        
+        self.storage = [None for x in range(self.capacity)]
 
 
     def get_num_slots(self):
@@ -29,18 +33,15 @@ class HashTable:
         Return the length of the list you're using to hold the hash
         table data. (Not the number of items stored in the hash table,
         but the number of slots in the main list.)
-
         One of the tests relies on this.
-
         Implement this.
         """
-        # Your code here
+        return self.capacity
 
 
     def get_load_factor(self):
         """
         Return the load factor for this hash table.
-
         Implement this.
         """
         # Your code here
@@ -49,7 +50,6 @@ class HashTable:
     def fnv1(self, key):
         """
         FNV-1 Hash, 64-bit
-
         Implement this, and/or DJB2.
         """
 
@@ -59,10 +59,14 @@ class HashTable:
     def djb2(self, key):
         """
         DJB2 hash, 32-bit
-
         Implement this, and/or FNV-1.
         """
-        # Your code here
+        h = 5381
+
+        for char in key:
+            h = ((h << 5) + h) + ord(char)
+
+        return h
 
 
     def hash_index(self, key):
@@ -76,41 +80,44 @@ class HashTable:
     def put(self, key, value):
         """
         Store the value with the given key.
-
         Hash collisions should be handled with Linked List Chaining.
-
         Implement this.
         """
-        # Your code here
+        loc = self.hash_index(key)
+        self.storage[loc] = value
+        # TODO for tomorrow: update to handle collisions
 
 
     def delete(self, key):
         """
         Remove the value stored with the given key.
-
         Print a warning if the key is not found.
-
         Implement this.
         """
-        # Your code here
+        loc = self.hash_index(key)
+        if self.storage[loc] is None:
+            raise LookupError('key does not exist in hash table')
+        else:
+            self.storage[loc] = None
 
 
     def get(self, key):
         """
         Retrieve the value stored with the given key.
-
         Returns None if the key is not found.
-
         Implement this.
         """
-        # Your code here
+        loc = self.hash_index(key)
+        if self.storage[loc] is None:
+            return None
+        else:
+            return self.storage[loc]
 
 
     def resize(self, new_capacity):
         """
         Changes the capacity of the hash table and
         rehashes all key/value pairs.
-
         Implement this.
         """
         # Your code here
