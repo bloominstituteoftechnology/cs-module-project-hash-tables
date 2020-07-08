2f9081a9162539f870d99c32510ae01d553e7080
Mike Sinnema <msinnema33@gmail.com>
2020-07-08 13:02:29 -0700

    working on markov

--- applications/markov/markov.py
+++ applications/markov/markov.py
@@ -1,13 +1,35 @@
 import random
 
 # Read in all the words in one go
-with open("input.txt") as f:
+with open("applications/markov/input.txt") as f:
     words = f.read()
+    inp = words.split() # splits input text into words
+    
 
 # TODO: analyze which words can follow other words
 # Your code here
-
-
+def make_pairs(arr):
+    for i in range(len(arr) - 1):
+        yield (arr[i], arr[i + 1])
+pairs = make_pairs(inp)
+word_dict = {}
+for word_1, word_2 in pairs:
+    if word_1 in word_dict.keys():
+        word_dict[word_1].append(word_2)
+    else:
+        word_dict[word_1] = [word_2]
+f_word = random.choice(inp)
+l_word = random.choice(inp)
+while f_word.islower() and f_word[0] != '"':
+    f_word = random.choice(inp)
+while l_word[-1] not in ['.', '!', '?']:
+    l_word = random.choice(inp)
+first = [f_word]
+last = [l_word]
+num_words = 150
+for i in range(num_words):
+    first.append(random.choice(word_dict[first[-1]]))
+print(' '.join(first + last))
 # TODO: construct 5 random sentences
 # Your code here
 
