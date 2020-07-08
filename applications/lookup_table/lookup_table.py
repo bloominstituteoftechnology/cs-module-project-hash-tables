bd84eda2969d494df45dc571cbf3e308cb792642
ElisabethShah <ElisabethShah@users.noreply.github.com>
2020-07-07 19:01:33 -0700

    Lookup table.

--- applications/lookup_table/lookup_table.py
+++ applications/lookup_table/lookup_table.py
@@ -1,4 +1,8 @@
-# Your code here
+import math
+import random
+
+
+lookup = {}
 
 
 def slowfun_too_slow(x, y):
@@ -9,13 +13,22 @@ def slowfun_too_slow(x, y):
 
     return v
 
+
 def slowfun(x, y):
     """
     Rewrite slowfun_too_slow() in here so that the program produces the same
     output, but completes quickly instead of taking ages to run.
     """
-    # Your code here
-
+    global lookup
+    if (x, y) in lookup:
+        return lookup[(x, y)]
+    else:
+        v = math.pow(x, y)
+        v = math.factorial(v)
+        v //= (x + y)
+        v %= 982451653
+        lookup[(x, y)] = v
+        return v
 
 
 # Do not modify below this line!
