6822373ef041e18beb880c637f8772c4a8c56b19
ElisabethShah <ElisabethShah@users.noreply.github.com>
2020-07-07 19:25:13 -0700

    Histogram.

--- applications/histo/histo.py
+++ applications/histo/histo.py
@@ -1,2 +1,25 @@
-# Your code here
+from collections import defaultdict
 
+
+def word_count(s):
+    ignore_chars = ',":;,.-+=/\\|[]{}()*^&'
+    counts = defaultdict(int)
+
+    s_clean = s.translate(str.maketrans('', '', ignore_chars))
+
+    for word in s_clean.lower().split():
+        counts[word] += 1
+
+    return counts
+
+
+with open('robin.txt') as file:
+    s = file.read()
+
+counts = word_count(s)
+width = max([len(word) for word in counts.keys()]) + 2
+
+counts = sorted(counts.items(), key=lambda x: (-x[1], x[0]))
+
+for count in counts:
+    print(f'{count[0].ljust(width)}' + '#' * count[1])
