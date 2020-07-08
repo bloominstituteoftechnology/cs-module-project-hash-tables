f527bb35491a83a648cd0914a9294278913f719d
Mike Sinnema <msinnema33@gmail.com>
2020-07-08 12:40:01 -0700

    no dups is working

--- applications/no_dups/no_dups.py
+++ applications/no_dups/no_dups.py
@@ -1,7 +1,13 @@
 def no_dups(s):
     # Your code here
-
-
+    non_dup_keys = []
+    if s == '':
+        return '' #returns an empty string if given
+    l = s.split(' ') #splits string into a list
+    a = dict.fromkeys(l)
+    for key in a.keys():
+        non_dup_keys.append(key)
+    return ' '.join(non_dup_keys)
 
 if __name__ == "__main__":
     print(no_dups(""))
