2457f6411b798597c09c9559caad62fcbe6c2a4c
ElisabethShah <ElisabethShah@users.noreply.github.com>
2020-07-07 19:11:11 -0700

    Word count.

--- applications/word_count/word_count.py
+++ applications/word_count/word_count.py
@@ -1,10 +1,21 @@
-def word_count(s):
-    # Your code here
+from collections import defaultdict
 
+
+def word_count(s):
+    ignore_chars = ',":;,.-+=/\\|[]{}()*^&'
+    counts = defaultdict(int)
+
+    s_clean = s.translate(str.maketrans('', '', ignore_chars))
+
+    for word in s_clean.lower().split():
+        counts[word] += 1
+
+    return counts
 
 
 if __name__ == "__main__":
     print(word_count(""))
     print(word_count("Hello"))
     print(word_count('Hello, my cat. And my cat doesn\'t say "hello" back.'))
-    print(word_count('This is a test of the emergency broadcast network. This is only a test.'))
+    print(word_count('This is a test of the emergency broadcast network. '
+                     'This is only a test.'))
