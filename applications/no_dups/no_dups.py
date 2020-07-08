dd7073d10a23cfb2847a98a8e137e2e954f1b4f8
josiah <josiahdamiwilliams@gmail.com>
2020-07-08 17:05:11 +0100

    finish no_dups

--- applications/no_dups/no_dups.py
+++ applications/no_dups/no_dups.py
@@ -1,5 +1,19 @@
 def no_dups(s):
     # Your code here
+    # List to hold non duplicates
+    lst_of_keys = []
+    if s == '':
+        # return empty string if it is given
+        return ''
+    # split string input into a list
+    l = s.split(' ')
+    # Turning array into dict as keys removes duplicates
+    a = dict.fromkeys(l)
+    for key in a.keys():
+        # Loops array into dict as keys removes duplicates
+        lst_of_keys.append(key)
+        # join it all for the return with a space between '' in order to maintain space distance
+    return ' ' .join(lst_of_keys)
 
 
 
