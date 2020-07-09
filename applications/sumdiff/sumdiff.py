016ee37eeaf90229376879e2fc3e67ab7bfd8200
Mike Sinnema <msinnema33@gmail.com>
2020-07-09 12:41:27 -0700

    sumdiff working

--- applications/sumdiff/sumdiff.py
+++ applications/sumdiff/sumdiff.py
@@ -3,8 +3,8 @@ find all a, b, c, d in q such that
 f(a) + f(b) = f(c) - f(d)
 """
 
-#q = set(range(1, 10))
-#q = set(range(1, 200))
+# q = set(range(1, 10))
+# q = set(range(1, 200))
 q = (1, 3, 4, 7, 12)
 
 
@@ -13,3 +13,13 @@ def f(x):
 
 # Your code here
 
+def sum_diff(q):
+    for i in q:
+        for j in q:
+            for k in q:
+                for l in q:
+                    if f(i) + f(j) == f(k) - f(l):
+                        print(f'{f(i)} + {f(j)} = {f(k)} - {f(l)}')
+
+
+print(sum_diff(q))
