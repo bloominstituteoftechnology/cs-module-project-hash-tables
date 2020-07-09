9d9bac0d496be5fad096a522390e7d5c47555721
Doina Langille <doina.jitoreanu@gmail.com>
2020-07-09 18:46:47 -0400

    day 4

--- applications/histo/histo.py
+++ applications/histo/histo.py
@@ -1,2 +1,25 @@
-# Your code here
+from collections import Counter
+
+with open("applications/histo/robin.txt", "r") as f:
+    content = f.read()
+
+s = content.translate(str.maketrans(content, content, ':,;.-+=/\|[]{}()*^&"'))
+
+backslash_strings = ["\n", "\t", "\r"]
+for bs in backslash_strings:
+    s = s.replace(bs, " ") 
+
+if len(s) == 0:
+    print("Text not found")
+
+words = [es.lower() for es in s.split(" ") if es is not ""]
+
+counts = Counter(words)
+indentation = len(max(words, key=len)) + 2
+sorted_list_of_words = sorted(counts, key=lambda k: (-counts[k], k))
+
+for word in sorted_list_of_words:
+    spacing = (indentation - len(word)) * " "
+    pound_signs = counts[word] * "#"
+    print(f"{word}{spacing}{pound_signs}")
 
--- applications/markov/markov.py
+++ applications/markov/markov.py
@@ -1,13 +1,37 @@
 import random
 
 # Read in all the words in one go
-with open("input.txt") as f:
+with open("applications/markov/input.txt") as f:
     words = f.read()
 
 # TODO: analyze which words can follow other words
-# Your code here
+split_words = words.split()
+dict = {}
+
+for i, word in enumerate(split_words):
+    if i+1 < len(split_words):
+        if word not in dict:
+            dict[word] = [split_words[i+1]]
+        else:
+            dict[word].append(split_words[i+1])
 
 
 # TODO: construct 5 random sentences
-# Your code here
+def generate():
+    sentence = []
+    while True:
+        starter = random.choice(list(dict.keys()))
+        if str(starter[0]).isupper() and not str(starter).endswith(".") and not str(starter).endswith("?") and not str(starter).endswith("!") and not str(starter).endswith("\""):
+            sentence.append(starter)
+            break
+    while True:
+        sentence_word = random.choice(dict[sentence[len(sentence)-1]])
+        if str(sentence_word).endswith(".") or str(sentence_word).endswith("?") or str(sentence_word).endswith("!") or str(sentence_word).endswith(".\"") or str(sentence_word).endswith("?\"") or str(sentence_word).endswith("!\""):
+            sentence.append(sentence_word)
+            break
+        sentence.append(sentence_word)
+    return ' '.join(sentence)
+
+for i in range(5):
+    print(generate())
 
