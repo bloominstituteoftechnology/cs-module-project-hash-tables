c96848e2ca78e559094c946c3e6b360efe47014a
Alex Pakalniskis <alexpakalniskis3@gmail.com>
2020-07-08 21:48:35 -0700

    complete all but sumdiff

--- applications/lookup_table/lookup_table.py
+++ applications/lookup_table/lookup_table.py
@@ -1,5 +1,9 @@
 # Your code here
+import math
+import random
 
+
+cache = {}
 
 def slowfun_too_slow(x, y):
     v = math.pow(x, y)
@@ -15,7 +19,15 @@ def slowfun(x, y):
     output, but completes quickly instead of taking ages to run.
     """
     # Your code here
-
+    if (x,y) in cache:
+        return cache[(x, y)]
+    else:
+        v = math.pow(x,y)
+        v = math.factorial(v)
+        v //= (x + y)
+        v %= 982451653
+        cache[(x,y)] = v
+        return v
 
 
 # Do not modify below this line!
