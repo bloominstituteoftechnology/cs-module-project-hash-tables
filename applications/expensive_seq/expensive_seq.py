7ff90bafe2012b89e45cc22f8d7144406806fff7
Pinefoot <jameslcarpino@gmail.com>
2020-07-09 12:02:02 -0700

    word_count passing

--- applications/expensive_seq/expensive_seq.py
+++ applications/expensive_seq/expensive_seq.py
@@ -2,23 +2,31 @@
 import math
 import random
 
+# cache {
+#     (0,0,0):0,
+#     (1,1,1): 73,
+# }
+
 
 cache = {}
 
 
 def expensive_seq(x, y, z):
     # Your code here
-    if x in cache:
-        return cache[x]
+    if (x, y, z) in cache:
+        return cache[(x, y, z)]
     elif x <= 0:
+        cache[(x, y, z)] = y + z
         return y + z
     else:
-        cache[x] = (
+        result = (
             expensive_seq(x - 1, y + 1, z)
             + expensive_seq(x - 2, y + 2, z * 2)
             + expensive_seq(x - 3, y + 3, z * 3)
         )
-    return cache[x]
+
+    cache[(x, y, z)] = result
+    return cache[(x, y, z)]
 
 
 if __name__ == "__main__":
