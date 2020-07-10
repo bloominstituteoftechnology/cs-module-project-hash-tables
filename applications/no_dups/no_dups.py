2d5125b29446f5b5dfc5eea70f48ff49705ae753
Shawn Gee <shawngee@gmail.com>
2020-07-10 13:11:50 -0400

    No dups and final pass of sumdiff

--- applications/no_dups/no_dups.py
+++ applications/no_dups/no_dups.py
@@ -1,8 +1,7 @@
 def no_dups(input_string):
-    # Your code here
     # Let's utilize a set to check for duplicates
     # We will loop through the string once and build our set of unique words
-    # Then we will loop through again and only add a word to our string if it is in our unique words set
+    # Then we will loop through again and only add a word to a result string if it is in our unique words set
     # Once a word is used, it will be removed from the unique words set
 
     split_words = input_string.split()
--- applications/sumdiff/sumdiff.py
+++ applications/sumdiff/sumdiff.py
@@ -3,11 +3,6 @@ find all a, b, c, d in q such that
 f(a) + f(b) = f(c) - f(d)
 """
 
-# q = set(range(1, 10))
-# q = set(range(1, 200))
-q = (1, 3, 4, 7, 12)
-
-
 def f(x):
     return x * 4 + 6
 
@@ -16,12 +11,16 @@ def f(x):
 # Let's try just looping through each of the combinations
 
 def print_combinations1(set):
+    output = ""
+
     for a in set:
         for b in set:
             for c in set:
                 for d in set:
                     if f(a) + f(b) == f(c) - f(d):
-                        print(f"f({a}) + f({b}) = f({c}) - f({d})")
+                        output += f"f({a}) + f({b}) = f({c}) - f({d})\n"
+    
+    print(output.count('\n'))
 
 
 # print_combinations1(q)
@@ -30,67 +29,48 @@ def print_combinations1(set):
 # We can get it down to O(n^2) if we store the results of summing in a dictionary
 # Then check against this dictionary with the diffs
 
-def print_combinations2(set):
+def print_combinations2(input_set):
     sums = {}
-    for a in set:
-        for b in set:
-            sum = f(a) + f(b)
+
+    output = ""
+
+    f_lut = {}
+
+    for num in input_set:
+        f_lut[num] = f(num)
+
+    for a in input_set:
+        for b in input_set:
+
+            sum = f_lut[a] + f_lut[b]
+
             if sum not in sums:
                 sums[sum] = [(a, b)]
             else:
                 sums[sum].append((a, b))
     
-    for c in set:
-        for d in set:
-            diff = f(c) - f(d)
-            if diff in sums:
-                for sum in sums[diff]:
-                   print(f"f({sum[0]}) + f({sum[1]}) = f({c}) - f({d})") 
-
-# print_combinations2(q)
-
-# We can further optimize since a + b is equivalent to b + a, so let's only check each pair once
-
-def print_combinations3(input_set: set):
-    sums = {}
-    b_set = input_set.copy()
-
-    for a in input_set:
-        for b in b_set:
-            sum = f(a) + f(b)
-            if sum not in sums:
-                sums[sum] = [(a, b), (b, a)]
-            else:
-                sums[sum].append([(a, b), (b, a)])
-
-        b_set.remove(a)
-            
     for c in input_set:
         for d in input_set:
-            diff = f(c) - f(d)
+            diff = f_lut[c] - f_lut[d]
             if diff in sums:
                 for sum in sums[diff]:
-                   print(f"f({sum[0]}) + f({sum[1]}) = f({c}) - f({d})") 
+                    output += f"f({sum[0]}) + f({sum[1]}) = f({c}) - f({d})\n"
+
+    print(output.count('\n'))
 
 
 import time
 
-q = set(range(1, 200))
+q = set(range(-20, 20))
 
-# start_time1 = time.time()
-# print_combinations1(q)
-# end_time1 = time.time()
+start_time1 = time.time()
+print_combinations1(q)
+end_time1 = time.time()
 
 start_time2 = time.time()
 print_combinations2(q)
 end_time2 = time.time()
 
-start_time3 = time.time()
-print_combinations3(q)
-end_time3 = time.time()
-
-# print(f"Combinations 1 finished in {end_time1 - start_time1}")
+print(f"Combinations 1 finished in {end_time1 - start_time1}")
 print(f"Combinations 2 finished in {end_time2 - start_time2}")
-print(f"Combinations 3 finished in {end_time3 - start_time3}")
-
 
