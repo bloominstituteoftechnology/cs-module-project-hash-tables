457b6a5a56e3a24eedbef64201fab55c76706587
Michael Stoffer <mstoffer@michaelstoffer.com>
2020-07-14 21:26:05 -0400

    Completed Word Count

--- applications/word_count/word_count.py
+++ applications/word_count/word_count.py
@@ -1,10 +1,23 @@
 def word_count(s):
-    # Your code here
+	# Your code here
+	cache = {}
+	new_s = s.lower()
+	ignore = '" : ; , . - + = / \ | [ ] { } ( ) * ^ &'.split(" ")
+	for char in ignore:
+		new_s = new_s.replace(char, "")
 
+	for word in new_s.split():
+		if word == "":
+			continue
+		if word not in cache:
+			cache[word] = 1
+		else:
+			cache[word] += 1
+	return cache
 
 
 if __name__ == "__main__":
-    print(word_count(""))
-    print(word_count("Hello"))
-    print(word_count('Hello, my cat. And my cat doesn\'t say "hello" back.'))
-    print(word_count('This is a test of the emergency broadcast network. This is only a test.'))
+	print(word_count(""))
+	print(word_count("Hello"))
+	print(word_count('Hello, my cat. And my cat doesn\'t say "hello" back.'))
+	print(word_count('This is a test of the emergency broadcast network. This is only a test.'))
