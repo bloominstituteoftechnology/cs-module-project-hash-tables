c36036270481444f8a1fa726802203e53ce06f67
Michael Stoffer <mstoffer@michaelstoffer.com>
2020-07-16 23:56:50 -0400

    Completed Sum and Difference

--- applications/sumdiff/sumdiff.py
+++ applications/sumdiff/sumdiff.py
@@ -9,7 +9,37 @@ q = (1, 3, 4, 7, 12)
 
 
 def f(x):
-    return x * 4 + 6
+	return x * 4 + 6
 
 # Your code here
+cache = {}
+for w in q:
+	for x in q:
+		for y in q:
+			for z in q:
+				if w not in cache:
+					l = f(w)
+					cache[w] = l
+				else:
+					l = cache[w]
 
+				if x not in cache:
+					l2 = f(x)
+					cache[x] = l2
+				else:
+					l2 = cache[x]
+
+				if y not in cache:
+					r = f(y)
+					cache[y] = r
+				else:
+					r = cache[y]
+
+				if z not in cache:
+					r2 = f(z)
+					cache[z] = r2
+				else:
+					r2 = cache[z]
+
+				if l + l2 == r - r2:
+					print('f({0}) + f({1}) = f({2}) - f({3})\t\t{4} + {5} = {6} - {7}'.format(w, x, y, z, l, l2, r, r2))
