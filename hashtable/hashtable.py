c505f6f1fc7e36f26f26f87a599b4b1340e7eaa3
Chance Rutledge <chancerutledge2@gmail.com>
2020-08-04 11:00:57 -0700

    day 1 mvp

--- hashtable/hashtable.py
+++ hashtable/hashtable.py
@@ -2,6 +2,7 @@ class HashTableEntry:
     """
     Linked List hash table key/value pair
     """
+
     def __init__(self, key, value):
         self.key = key
         self.value = value
@@ -21,8 +22,9 @@ class HashTable:
     """
 
     def __init__(self, capacity):
-        # Your code here
-
+        self.capacity = capacity
+        self.bucket_array = [None] * capacity
+        self.size = 0
 
     def get_num_slots(self):
         """
@@ -34,8 +36,7 @@ class HashTable:
 
         Implement this.
         """
-        # Your code here
-
+        return len(self.bucket_array)
 
     def get_load_factor(self):
         """
@@ -43,8 +44,7 @@ class HashTable:
 
         Implement this.
         """
-        # Your code here
-
+        return (self.size / self.capacity)
 
     def fnv1(self, key):
         """
@@ -55,22 +55,23 @@ class HashTable:
 
         # Your code here
 
-
     def djb2(self, key):
         """
         DJB2 hash, 32-bit
 
         Implement this, and/or FNV-1.
         """
-        # Your code here
-
+        hash_var = 5381
+        for i in key:
+            hash = ((hash << 5) + hash) + ord(i)
+        return hash & 0xFFFFFFFF
 
     def hash_index(self, key):
         """
         Take an arbitrary key and return a valid integer index
         between within the storage capacity of the hash table.
         """
-        #return self.fnv1(key) % self.capacity
+        # return self.fnv1(key) % self.capacity
         return self.djb2(key) % self.capacity
 
     def put(self, key, value):
@@ -81,8 +82,8 @@ class HashTable:
 
         Implement this.
         """
-        # Your code here
-
+        bucket_index = self.hash_index(key)
+        self.bucket_array[bucket_index] = value
 
     def delete(self, key):
         """
@@ -92,8 +93,12 @@ class HashTable:
 
         Implement this.
         """
-        # Your code here
-
+        bucket_index = self.hash_index(key)
+        existing_entry = self.bucket_array[bucket_index]
+        if existing_entry:
+            self.bucket_array[bucket_index] = None
+        else:
+            print("Key not found")
 
     def get(self, key):
         """
@@ -103,8 +108,12 @@ class HashTable:
 
         Implement this.
         """
-        # Your code here
-
+        bucket_index = self.hash_index(key)
+        existing_entry = self.bucket_array[bucket_index]
+        if existing_entry:
+            return self.bucket_array[bucket_index]
+        else:
+            return None
 
     def resize(self, new_capacity):
         """
@@ -114,7 +123,6 @@ class HashTable:
         Implement this.
         """
         # Your code here
-
 
 
 if __name__ == "__main__":
