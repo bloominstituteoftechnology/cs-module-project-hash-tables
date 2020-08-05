f1919197dbda12631b6411edd28bb6e01c471eb7
Michael Lowe <mrlowememphis@yahoo.com>
2020-08-04 20:31:13 -0500

    Day 1 MVP

--- hashtable/hashtable.py
+++ hashtable/hashtable.py
@@ -22,6 +22,9 @@ class HashTable:
 
     def __init__(self, capacity):
         # Your code here
+        self.capacity = capacity
+        self.hashTable = [None] * capacity
+        #added day 1
 
 
     def get_num_slots(self):
@@ -35,6 +38,7 @@ class HashTable:
         Implement this.
         """
         # Your code here
+        return self.capacity
 
 
     def get_load_factor(self):
@@ -44,6 +48,8 @@ class HashTable:
         Implement this.
         """
         # Your code here
+        # return self.count / self.capacity
+      
 
 
     def fnv1(self, key):
@@ -63,6 +69,11 @@ class HashTable:
         Implement this, and/or FNV-1.
         """
         # Your code here
+        hash = 5381
+        for x in key:
+            hash = ((hash << 5) + hash) + ord(x)
+        return hash
+        #added day 1
 
 
     def hash_index(self, key):
@@ -82,6 +93,10 @@ class HashTable:
         Implement this.
         """
         # Your code here
+        new_entry = HashTableEntry(key, value)
+        idx = self.hash_index(key)
+        self.hashTable[idx] = new_entry
+        #added day 1
 
 
     def delete(self, key):
@@ -93,6 +108,9 @@ class HashTable:
         Implement this.
         """
         # Your code here
+        idx = self.hash_index(key)
+        self.hashTable[idx].value = None
+        #added day 1
 
 
     def get(self, key):
@@ -104,6 +122,10 @@ class HashTable:
         Implement this.
         """
         # Your code here
+        idx = self.hash_index(key)
+        print('self', self.hashTable[idx])
+        return self.hashTable[idx].value
+        #added day 1
 
 
     def resize(self, new_capacity):
@@ -114,6 +136,10 @@ class HashTable:
         Implement this.
         """
         # Your code here
+        table = HashTable(10000)
+        print(table.put('hello', 'world'))
+        print(table.get('hello'))
+        #added day 1
 
 
 
