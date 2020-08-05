55c5642d8bfeb264c12819eb1659beeb1775cc45
Nhan <natenguyen51294@gmail.com>
2020-08-05 14:30:16 -0500

    updated

--- applications/expensive_seq/expensive_seq.py
+++ applications/expensive_seq/expensive_seq.py
@@ -6,10 +6,10 @@ cache = {}
 def expensive_seq(x, y, z):
     if x <= 0:
         return y + z
-    if x not in cache:
-        cache[x] = expensive_seq(x - 1, y + 1, z) + expensive_seq(x-2,
-                                                                  y+2, z*2) + expensive_seq(x-3, y+3, z*3)
-    return cache[x]
+    if (x, y, z) not in cache:
+        cache[(x, y, z)] = expensive_seq(
+            x-1, y+1, z) + expensive_seq(x-2, y+2, z*2) + expensive_seq(x-3, y+3, z*3)
+    return cache[(x, y, z)]
 
 
 if __name__ == "__main__":
