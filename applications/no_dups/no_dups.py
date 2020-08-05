2306536ddefa433c06e4b35d818d9e1418019afe
wesley.moody <wgm3190@gmail.com>
2020-08-05 16:40:57 -0500

    no dups passing test

--- applications/no_dups/no_dups.py
+++ applications/no_dups/no_dups.py
@@ -1,5 +1,21 @@
+
+
 def no_dups(s):
-    # Your code here
+    #List to hold non duplicates 
+    lst_of_keys = []
+    if s == '':
+        # Return empty string if it is given
+        return ''
+    # Split string input into a list
+    l = s.split(' ')
+    # Turning array into dict as keys removes duplicates
+    a = dict.fromkeys(l)
+    for key in a.keys():
+        # Loops keys and appends them to List of keys
+        lst_of_keys.append(key)
+        #Join it all for the return with a space between ' ' in order to maintain space distance
+    return ' '.join(lst_of_keys)
+        
 
 
 
