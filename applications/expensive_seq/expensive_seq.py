ec47d8c4ca71ff2b6ef5ce61624b4cda4e174c63
eriksandviksea <eriksandvikSEA@Gmail.com>
2020-08-05 16:05:29 -0700

    expensive_seq passing

--- applications/expensive_seq/expensive_seq.py
+++ applications/expensive_seq/expensive_seq.py
@@ -1,14 +1,25 @@
 # Your code here
 
+memo = {}
+
 
 def expensive_seq(x, y, z):
     # Your code here
-
+    if x <= 0:
+        return y + z
+    if x > 0:
+        if (x, y, z) not in memo:
+            memo[(x, y, z)] = (
+                expensive_seq(x - 1, y + 1, z)
+                + expensive_seq(x - 2, y + 2, z * 2)
+                + expensive_seq(x - 3, y + 3, z * 3)
+            )
+    return memo[(x, y, z)]
 
 
 if __name__ == "__main__":
     for i in range(10):
-        x = expensive_seq(i*2, i*3, i*4)
+        x = expensive_seq(i * 2, i * 3, i * 4)
         print(f"{i*2} {i*3} {i*4} = {x}")
 
     print(expensive_seq(150, 400, 800))
