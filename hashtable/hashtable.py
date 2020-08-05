24f7b54aa97150bbf74e4eafbc7c6ddc08e2544d
Aaron Watkins <61791690+ekselan@users.noreply.github.com>
2020-08-05 00:50:18 -0400

    linked list hashtable passing _no_collisions test

--- hashtable/hashtable.py
+++ hashtable/hashtable.py
@@ -112,27 +112,25 @@ class HashTable:
         # if there is already value for key
         else:
             # handle collisions -> check if head node has a next
-            if self.contents[index].next is None:
-                # breakpoint()
-                # if head node but no next, make head node's next the new node
-                self.contents[index].next = new
-                self.count += 1
+            # special case - if key matches input key, then update
+            # value and don't set a next
+            if contents.key == key:
+                contents.value = new.value
 
-            # otherwise, there is a next, so make a new next
             else:
-                
-                cur = self.contents[index].next
-                cur.next = new
-                self.count += 1
 
-        #     # update value in key
-        #     self.contents[index] = new
-        #     # update next via Linked List Chaining as rest of contents
-        #     self.contents[index].next = contents
-        
-        # # otherwise, there are no contents, so simply enter new value
-        # else:
-        #     self.contents[index] = new
+                if contents.next is None:
+                    # breakpoint()
+                    # if head node but no next, make head node's next the new node
+                    contents.next = new
+                    self.count += 1
+
+                # otherwise, there is a next, so make a new next
+                else:
+                    
+                    cur = self.contents[index].next
+                    cur.next = new
+                    self.count += 1
 
 
     def delete(self, key):
@@ -266,37 +264,56 @@ if __name__ == "__main__":
     # return_value = ht.get("key-2")
     # print("key-2 val:", return_value) #> None
 
-    ht = HashTable(8)
+    # ht = HashTable(8)
+
+    # ht.put("key-0", "val-0")
+    # ht.put("key-1", "val-1")
+    # ht.put("key-2", "val-2")
+    # ht.put("key-3", "val-3")
+    # ht.put("key-4", "val-4")
+    # ht.put("key-5", "val-5")
+    # ht.put("key-6", "val-6")
+    # ht.put("key-7", "val-7")
+    # ht.put("key-8", "val-8")
+    # ht.put("key-9", "val-9")
+
+    # return_value = ht.get("key-0")
+    # print("key-0 val:", return_value) #> val-0
+    # return_value = ht.get("key-1")
+    # print("key-1 val:", return_value) #> val-1
+    # return_value = ht.get("key-2")
+    # print("key-2 val:", return_value) #> val-2
+    # return_value = ht.get("key-3")
+    # print("key-3 val:", return_value) #> val-3
+    # return_value = ht.get("key-4")
+    # print("key-4 val:", return_value) #> val-4
+    # return_value = ht.get("key-5")
+    # print("key-5 val:", return_value) #> val-5
+    # return_value = ht.get("key-6")
+    # print("key-6 val:", return_value) #> val-6
+    # return_value = ht.get("key-7")
+    # print("key-7 val:", return_value) #> val-7
+
+    # return_value = ht.get("key-8")
+    # print("key-8 val:", return_value) #> val-8 
+    # return_value = ht.get("key-9")
+    # print("key-9 val:", return_value) #> val-9 
+
+    ht = HashTable(0x10000)
 
     ht.put("key-0", "val-0")
     ht.put("key-1", "val-1")
     ht.put("key-2", "val-2")
-    ht.put("key-3", "val-3")
-    ht.put("key-4", "val-4")
-    ht.put("key-5", "val-5")
-    ht.put("key-6", "val-6")
-    ht.put("key-7", "val-7")
-    ht.put("key-8", "val-8")
-    ht.put("key-9", "val-9")
+
+    # breakpoint()
+
+    ht.put("key-0", "new-val-0")
+    ht.put("key-1", "new-val-1")
+    ht.put("key-2", "new-val-2")
 
     return_value = ht.get("key-0")
-    print("key-0 val:", return_value) #> val-0
+    print("key-0 val:", return_value) #> "new-val-0"
     return_value = ht.get("key-1")
-    print("key-1 val:", return_value) #> val-1
+    print("key-1 val:", return_value) #> "new-val-1"
     return_value = ht.get("key-2")
-    print("key-2 val:", return_value) #> val-2
-    return_value = ht.get("key-3")
-    print("key-3 val:", return_value) #> val-3
-    return_value = ht.get("key-4")
-    print("key-4 val:", return_value) #> val-4
-    return_value = ht.get("key-5")
-    print("key-5 val:", return_value) #> val-5
-    return_value = ht.get("key-6")
-    print("key-6 val:", return_value) #> val-6
-    return_value = ht.get("key-7")
-    print("key-7 val:", return_value) #> val-7
-
-    return_value = ht.get("key-8")
-    print("key-8 val:", return_value) #> val-8 
-    return_value = ht.get("key-9")
-    print("key-9 val:", return_value) #> val-9 
+    print("key-2 val:", return_value) #> "new-val-2"
