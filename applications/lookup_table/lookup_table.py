9619525add35c264a94db307fbe2b8eacc1e3dd4
Jon Nguyen <jon.mail@hotmail.com>
2020-08-05 12:16:44 -0700

    chugging along on problems

--- applications/lookup_table/lookup_table.py
+++ applications/lookup_table/lookup_table.py
@@ -1,4 +1,6 @@
 # Your code here
+import math
+import random
 
 
 def slowfun_too_slow(x, y):
@@ -8,13 +10,21 @@ def slowfun_too_slow(x, y):
     v %= 982451653
 
     return v
-
+cache = {}
 def slowfun(x, y):
     """
     Rewrite slowfun_too_slow() in here so that the program produces the same
     output, but completes quickly instead of taking ages to run.
     """
     # Your code here
+    if (x, y) in cache:
+        return cache[(x,y)]
+    else:
+        v = slowfun_too_slow(x,y)
+        cache[(x, y)] = v
+        return v
+        
+
 
 
 
