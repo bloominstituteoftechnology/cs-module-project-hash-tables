6846593df77b88334fd0642f6eaf44e1ecf903b4
Matthew Bolton <matthewwbolton@gmail.com>
2020-08-05 19:01:42 -0400

    Update markov.py

--- applications/markov/markov.py
+++ applications/markov/markov.py
@@ -2,12 +2,15 @@ import random
 
 # Read in all the words in one go
 with open("input.txt") as f:
-    words = f.read()
+    words = f.read().strip().split().replace("\n", " ")
+    print(words)
 
 # TODO: analyze which words can follow other words
 # Your code here
 
+d = dict()
 
-# TODO: construct 5 random sentences
-# Your code here
+for word, index in enumerate(words):
 
+    # TODO: construct 5 random sentences
+    # Your code here
