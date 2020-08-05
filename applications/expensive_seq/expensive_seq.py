1ab288ffe9ef53d5ed4974b156313e8fd3e81b91
Matthew Bolton <matthewwbolton@gmail.com>
2020-08-05 18:06:37 -0400

    -completed word_count.py, lookup_table.py and expensive_sequence.py and are currently passing all tests

--- applications/expensive_seq/expensive_seq.py
+++ applications/expensive_seq/expensive_seq.py
@@ -1,9 +1,29 @@
 # Your code here
 
+# def exps(x, y, z):
+#     if x <= 0:
+#         y + z
 
-def expensive_seq(x, y, z):
+#     if x > 0:
+#          exps(x-1, y+1, z) + exps(x-2, y+2, z*2) + exps(x-3, y+3, z*3)
+
+
+def expensive_seq(x, y, z, cache=dict()):
     # Your code here
 
+    if x <= 0:
+        return y + z
+
+    if (x, y, z) not in cache:
+
+        cache[(x, y, z)] = expensive_seq(x - 1, y + 1, z, cache) + expensive_seq(
+            x - 2, y + 2, z * 2, cache) + expensive_seq(x - 3, y + 3, z * 3, cache)
+
+        return cache[(x, y, z)]
+
+    else:
+
+        return cache[(x, y, z)]
 
 
 if __name__ == "__main__":
