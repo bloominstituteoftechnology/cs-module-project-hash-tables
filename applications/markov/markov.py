bc4701347309bcc9276f37c65ec24ded9202cebb
Nhan <natenguyen51294@gmail.com>
2020-08-05 17:32:51 -0500

    look up table done

--- applications/markov/markov.py
+++ applications/markov/markov.py
@@ -4,10 +4,16 @@ import random
 with open("input.txt") as f:
     words = f.read()
 
+    my_list = []
+    words = words.split()
+    # print(words)
+    for i in range(len(words) - 1):
+        my_list.append((words[i], words[i + 1]))
+
+
 # TODO: analyze which words can follow other words
 # Your code here
 
 
 # TODO: construct 5 random sentences
 # Your code here
-
