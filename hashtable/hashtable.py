76bb35fc4ba42c95c699ffa31bc0b06834f04e41
stevekutz <stkutz@gmail.com>
2020-08-04 21:51:05 -0500

    init, built _dvb2 hash

--- hashtable/hashtable.py
+++ hashtable/hashtable.py
@@ -1,11 +1,13 @@
 class HashTableEntry:
-    """
-    Linked List hash table key/value pair
-    """
-    def __init__(self, key, value):
-        self.key = key
-        self.value = value
-        self.next = None
+     """
+     Linked List hash table key/value pair
+     """
+     def __init__(self, key, value):
+          # self.key = key
+          # self.value = value
+          # self.next = None
+          self.key = key   # Day 1, index
+          self.value = value # Day1, element value
 
 
 # Hash table can't have fewer than this many slots
@@ -13,141 +15,152 @@ MIN_CAPACITY = 8
 
 
 class HashTable:
-    """
-    A hash table that with `capacity` buckets
-    that accepts string keys
+     """
+     A hash table that with `capacity` buckets
+     that accepts string keys
 
-    Implement this.
-    """
+     Implement this.
+     """
 
-    def __init__(self, capacity):
-        # Your code here
-
-
-    def get_num_slots(self):
-        """
-        Return the length of the list you're using to hold the hash
-        table data. (Not the number of items stored in the hash table,
-        but the number of slots in the main list.)
-
-        One of the tests relies on this.
-
-        Implement this.
-        """
-        # Your code here
+     def __init__(self, capacity):
+          self.capacity = MIN_CAPACITY  # Day 1, size here is same a number of slots, until wwe deal with collisions
+          self.slots = [ None for item in range(self.capacity)]
 
 
-    def get_load_factor(self):
-        """
-        Return the load factor for this hash table.
 
-        Implement this.
-        """
-        # Your code here
+     def get_num_slots(self):
+          """
+          Return the length of the list you're using to hold the hash
+          table data. (Not the number of items stored in the hash table,
+          but the number of slots in the main list.)
 
+          One of the tests relies on this.
 
-    def fnv1(self, key):
-        """
-        FNV-1 Hash, 64-bit
-
-        Implement this, and/or DJB2.
-        """
-
-        # Your code here
+          Implement this.
+          """
+          # Your code here
 
 
-    def djb2(self, key):
-        """
-        DJB2 hash, 32-bit
+     def get_load_factor(self):
+          """
+          Return the load factor for this hash table.
 
-        Implement this, and/or FNV-1.
-        """
-        # Your code here
-
-
-    def hash_index(self, key):
-        """
-        Take an arbitrary key and return a valid integer index
-        between within the storage capacity of the hash table.
-        """
-        #return self.fnv1(key) % self.capacity
-        return self.djb2(key) % self.capacity
-
-    def put(self, key, value):
-        """
-        Store the value with the given key.
-
-        Hash collisions should be handled with Linked List Chaining.
-
-        Implement this.
-        """
-        # Your code here
+          Implement this.
+          """
+          # Your code here
 
 
-    def delete(self, key):
-        """
-        Remove the value stored with the given key.
+     def fnv1(self, key):
+          """
+          FNV-1 Hash, 64-bit
 
-        Print a warning if the key is not found.
+          Implement this, and/or DJB2.
+          """
 
-        Implement this.
-        """
-        # Your code here
-
-
-    def get(self, key):
-        """
-        Retrieve the value stored with the given key.
-
-        Returns None if the key is not found.
-
-        Implement this.
-        """
-        # Your code here
+          # Your code here
 
 
-    def resize(self, new_capacity):
-        """
-        Changes the capacity of the hash table and
-        rehashes all key/value pairs.
+     def djb2(self, key):
+          """
+          DJB2 hash, 32-bit
 
-        Implement this.
-        """
-        # Your code here
+          Implement this, and/or FNV-1.
+          """
+          # Your code here
+          salt = 5381
+
+          for item in key:
+               hash_val = ((salt<<5) + salt) + ord(item)
+
+          return hash_val     
+
+     def hash_index(self, key):
+          """
+          Take an arbitrary key and return a valid integer index
+          between within the storage capacity of the hash table.
+          """
+          #return self.fnv1(key) % self.capacity
+          return self.djb2(key) % self.capacity
+
+     def put(self, key, value):
+          """
+          Store the value with the given key.
+
+          Hash collisions should be handled with Linked List Chaining.
+
+          Implement this.
+          """
+          # Your code here
+
+
+     def delete(self, key):
+          """
+          Remove the value stored with the given key.
+
+          Print a warning if the key is not found.
+
+          Implement this.
+          """
+          # Your code here
+
+
+     def get(self, key):
+          """
+          Retrieve the value stored with the given key.
+
+          Returns None if the key is not found.
+
+          Implement this.
+          """
+          # Your code here
+
+
+     def resize(self, new_capacity):
+          """
+          Changes the capacity of the hash table and
+          rehashes all key/value pairs.
+
+          Implement this.
+          """
+          # Your code here
 
 
 
 if __name__ == "__main__":
-    ht = HashTable(8)
+     ht = HashTable(8)
 
-    ht.put("line_1", "'Twas brillig, and the slithy toves")
-    ht.put("line_2", "Did gyre and gimble in the wabe:")
-    ht.put("line_3", "All mimsy were the borogoves,")
-    ht.put("line_4", "And the mome raths outgrabe.")
-    ht.put("line_5", '"Beware the Jabberwock, my son!')
-    ht.put("line_6", "The jaws that bite, the claws that catch!")
-    ht.put("line_7", "Beware the Jubjub bird, and shun")
-    ht.put("line_8", 'The frumious Bandersnatch!"')
-    ht.put("line_9", "He took his vorpal sword in hand;")
-    ht.put("line_10", "Long time the manxome foe he sought--")
-    ht.put("line_11", "So rested he by the Tumtum tree")
-    ht.put("line_12", "And stood awhile in thought.")
+     # ht.put("line_1", "'Twas brillig, and the slithy toves")
+     # ht.put("line_2", "Did gyre and gimble in the wabe:")
+     # ht.put("line_3", "All mimsy were the borogoves,")
+     # ht.put("line_4", "And the mome raths outgrabe.")
+     # ht.put("line_5", '"Beware the Jabberwock, my son!')
+     # ht.put("line_6", "The jaws that bite, the claws that catch!")
+     # ht.put("line_7", "Beware the Jubjub bird, and shun")
+     # ht.put("line_8", 'The frumious Bandersnatch!"')
+     # ht.put("line_9", "He took his vorpal sword in hand;")
+     # ht.put("line_10", "Long time the manxome foe he sought--")
+     # ht.put("line_11", "So rested he by the Tumtum tree")
+     # ht.put("line_12", "And stood awhile in thought.")
 
-    print("")
+     # print("")
 
-    # Test storing beyond capacity
-    for i in range(1, 13):
-        print(ht.get(f"line_{i}"))
+     # # Test storing beyond capacity
+     # for i in range(1, 13):
+     #      print(ht.get(f"line_{i}"))
 
-    # Test resizing
-    old_capacity = ht.get_num_slots()
-    ht.resize(ht.capacity * 2)
-    new_capacity = ht.get_num_slots()
+     # # Test resizing
+     # old_capacity = ht.get_num_slots()
+     # ht.resize(ht.capacity * 2)
+     # new_capacity = ht.get_num_slots()
 
-    print(f"\nResized from {old_capacity} to {new_capacity}.\n")
+     # print(f"\nResized from {old_capacity} to {new_capacity}.\n")
 
-    # Test if data intact after resizing
-    for i in range(1, 13):
-        print(ht.get(f"line_{i}"))
+     # # Test if data intact after resizing
+     # for i in range(1, 13):
+     #      print(ht.get(f"line_{i}"))
 
-    print("")
+     # print("")
+
+
+print(f' ht capacity  is >> {ht.capacity} ')
+
