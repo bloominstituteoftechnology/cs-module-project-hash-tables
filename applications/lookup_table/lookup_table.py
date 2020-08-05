f60ddca845d5a4194c9f120c59899bf896e81de6
Aaron Watkins <61791690+ekselan@users.noreply.github.com>
2020-08-05 19:07:29 -0400

    expensive seq and lookup table

--- applications/lookup_table/lookup_table.py
+++ applications/lookup_table/lookup_table.py
@@ -1,4 +1,6 @@
 # Your code here
+import math
+import random
 
 
 def slowfun_too_slow(x, y):
@@ -9,17 +11,29 @@ def slowfun_too_slow(x, y):
 
     return v
 
+
+# Add cache
+cache = {}
+
+
 def slowfun(x, y):
     """
     Rewrite slowfun_too_slow() in here so that the program produces the same
     output, but completes quickly instead of taking ages to run.
     """
     # Your code here
+    # if tuple in cache, return it
+    if (x, y) in cache:
+        return cache[(x, y)]
 
+    # otherwise, item not in cache so hash it
+    else:
+        v = slowfun_too_slow(x, y)
+        cache[(x, y)] = v
+        return v
 
 
 # Do not modify below this line!
-
 for i in range(50000):
     x = random.randrange(2, 14)
     y = random.randrange(3, 6)
