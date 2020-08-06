fe5194d250e0deb512e81390b1c4fb233d34e2fa
Nick <ptnguyen1901@gmail.com>
2020-08-06 15:57:45 -0400

    expensive_sequence

--- applications/expensive_seq/expensive_seq.py
+++ applications/expensive_seq/expensive_seq.py
@@ -1,14 +1,22 @@
 # Your code here
-
+cache = dict()
 
 def expensive_seq(x, y, z):
-    # Your code here
+    tuple = (x, y, z)
 
+    if tuple not in cache:
+        if x <= 0:
+            cache[tuple] = y + z
+        if x > 0:
+            cache[tuple] = expensive_seq(x - 1, y + 1, z) + expensive_seq(x - 2, y + 2, z * 2) + expensive_seq(x - 3, y + 3, z * 3)
+
+    return  cache[tuple]
 
 
 if __name__ == "__main__":
     for i in range(10):
-        x = expensive_seq(i*2, i*3, i*4)
-        print(f"{i*2} {i*3} {i*4} = {x}")
+        x = expensive_seq(i * 2, i * 3, i * 4)
+
+        print(f"{i * 2} {i * 3} {i * 4} = {x}")
 
     print(expensive_seq(150, 400, 800))
