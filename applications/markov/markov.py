d3416cafa35ad256583e8d1a2066f53024155e17
Matthew Bolton <matthewwbolton@gmail.com>
2020-08-06 17:11:14 -0400

    -completed histo.py

--- applications/markov/markov.py
+++ applications/markov/markov.py
@@ -10,21 +10,40 @@ with open("input.txt") as f:
 
 d = dict()
 
-for index, word in enumerate(words):
-    if index < (len(words)-1):
-        if word == "":
-            word = " "
-        elif words[index + 1] == "":
-            d[word] = " "
-        else:
-            d[word] = words[index + 1]
+for i in range(len(words) - 1):
+    word = words[i]
+    next_word = words[i + 1]
+
+    if word not in d:
+        d[word] = [next_word]
+
+    else:
+        d[word].append(next_word)
+
 # print(d)
 
 
 # TODO: construct 5 random sentences
 # Your code here
 
-def make_sentence():
+start_list = []
 
-    try:
-        start = get_starting_word()
+for key in d.keys():
+    if key[0].isupper() and len(key) > 1 or key[1].isupper():
+        start_list.append(key)
+
+random_word = random.choice(start_list)
+
+stopped = False
+
+stop_list = '.?!'
+
+while not stopped:
+    print(random_word, end=" ")
+
+    if random_word[-1] in stop_list and len(random_word) > 1 or random_word[-2] in stop_list:
+        stopped = True
+
+    words_to_follow = d[random_word]
+
+    follow_word = random.choice(words_to_follow)
