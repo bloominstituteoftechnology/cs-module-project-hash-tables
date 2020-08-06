613cd100ef6981b4bf2d648b543d49e258c4cc33
frances-hansen <francesjuniperhansen@users.noreply.github.com>
2020-08-06 16:34:49 -0700

    in progress

--- hashtable/hashtable.py
+++ hashtable/hashtable.py
@@ -6,10 +6,61 @@ class HashTableEntry:
         self.key = key
         self.value = value
         self.next = None
+        
+    def __repr__(self):
+        return repr(self.key)
 
 
-# Hash table can't have fewer than this many slots
-MIN_CAPACITY = 8
+
+class LL:
+    def __init__(self):
+        self.head = None
+
+    # this basically goes through the list until the matching key is found
+    # if no matching key is found it returns None
+    def find_current_node(self,key):
+        curr = self.head
+
+        while curr is not None:
+            
+            if curr.key == key:
+                
+                return curr
+            
+            curr = curr.next
+            
+        return None
+
+    def add_to_head(self,key,value):
+        node = HashTableEntry(key,value)
+        # this switches the pointers around so that the current head can switch
+        if self.head is not None:
+            node.next = self.head
+
+        self.head = node
+
+    def delete(self,key):
+        curr = self.head
+        # if the node is already None theres no reason to change anything
+        if curr is None:
+            return None
+        # if the key matches for head we can modify pointers for deletion
+        if curr.key == key:
+            self.head = curr.next
+            return curr
+        # this is for deleting non head nodes
+        else:
+            prev = curr
+            curr = curr.next
+
+            while curr is not None:
+                if curr.key == key:
+                    # this turns pointers away from current for deletion
+                    prev.next = curr.next
+                    return curr 
+            return None
+
+        
 
 
 class HashTable:
@@ -22,6 +73,13 @@ class HashTable:
 
     def __init__(self, capacity):
         # Your code here
+        self.capacity = capacity
+        self.hash_table = [None] * capacity
+        self.count = 0
+
+        for x in range(self.capacity):
+            self.hash_table[x] = LL()
+        
 
 
     def get_num_slots(self):
@@ -35,6 +93,7 @@ class HashTable:
         Implement this.
         """
         # Your code here
+        return self.capacity
 
 
     def get_load_factor(self):
@@ -44,6 +103,7 @@ class HashTable:
         Implement this.
         """
         # Your code here
+        return self.count / self.capacity
 
 
     def fnv1(self, key):
@@ -63,6 +123,10 @@ class HashTable:
         Implement this, and/or FNV-1.
         """
         # Your code here
+        hashed = 5381
+        for c in key:
+            hashed = (hashed * 33) + ord(c)
+        return hashed       
 
 
     def hash_index(self, key):
@@ -82,7 +146,25 @@ class HashTable:
         Implement this.
         """
         # Your code here
+        load_factor = self.get_load_factor()
 
+        # this doubles the hash map if the number of occupied space exceeds 70%
+        if load_factor > 0.7:
+            self.resize(self.capacity * 2)
+
+        index = self.hash_index(key)
+            # this is to check for existing nodes to avoid clash
+
+        search_node = self.hash_table[index].find_current_node(key)
+        # if the head is not empty we set it to the current value to place
+        if search_node is not None:
+            search_node.value = value
+            
+        # otherwise we set the head and increase count
+        else:
+            self.hash_table[index].add_to_head(key,value)
+            #print("TABLE: \n",self.hash_table)
+            self.count+=1
 
     def delete(self, key):
         """
@@ -93,6 +175,15 @@ class HashTable:
         Implement this.
         """
         # Your code here
+        index = self.hash_index(key)
+
+        deleted = self.hash_table[index].delete(key)
+
+        if deleted is None:
+            print('Key {} not found!'.format(key))
+        else:
+            print('Key {} deleted'.format(key))
+            self.count -= 1
 
 
     def get(self, key):
@@ -104,6 +195,15 @@ class HashTable:
         Implement this.
         """
         # Your code here
+        index = self.hash_index(key)
+        
+        # we check for the key using the hash index 
+        LL_res = self.hash_table[index].find_current_node(key)
+        # we check if the key can be found. if not it returns None
+        # if it is found it returns the matching value
+        if LL_res is None:
+            return None
+        return LL_res.value
 
 
     def resize(self, new_capacity):
@@ -115,7 +215,44 @@ class HashTable:
         """
         # Your code here
 
+        # need to save old values
+        old_hash_table = self.hash_table
 
+        # make a new table for resized list
+        new_hash_table = [None] * new_capacity
+
+        self.count = 0
+
+        for x in range(new_capacity):
+            new_hash_table[x] = LL()
+##            print("X_NEW_CAP \n",x)
+##            print("NEW_TBL: \n",new_hash_table)
+
+        self.hash_table = new_hash_table
+
+        self.capacity = new_capacity
+
+        self.count = 0
+        # iterate through the old hash table
+        for y in old_hash_table:
+            curr = y.head
+            
+            # this keeps going until the next node is None. which means that
+            # there are no more nodes to add
+            while curr is not None:
+                # this finally stores the new array
+                self.put(curr.key,curr.value)
+
+                curr = curr.next
+
+    def __str__(self):
+
+        return "".join(str(item) for item in self.hash_table)
+
+
+
+# Hash table can't have fewer than this many slots
+MIN_CAPACITY = 8
 
 if __name__ == "__main__":
     ht = HashTable(8)
