8c2f5cc49b6c7978e80d94883181f6378e973c09
wesley.moody <wgm3190@gmail.com>
2020-08-06 10:31:42 -0500

    exp_seq

--- applications/expensive_seq/expensive_seq.py
+++ applications/expensive_seq/expensive_seq.py
@@ -8,19 +8,14 @@ def expensive_seq(x, y, z):
     # base case
     if x <= 0:
         return y + z
-    # see if value(tuple)  has been added to the cache
+    # see if value (tuple) has been added to the cache
     if x > 0:
         if (x, y, z) not in cache:
             # if not in cache add it with math
             cache[(x, y, z)] = expensive_seq(x-1, y+1, z) + expensive_seq(x-2, y+2, z*2) + expensive_seq(x-3, y+3, z*3)
             return cache[(x, y, z)]
         # return the value from cache
-        return cache[(x, y, z)] 
-
-
-
-
-
+        return cache[(x, y, z)]
 
 
 
