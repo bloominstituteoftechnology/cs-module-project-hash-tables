955c72361bf1e9fec1ceb6cfbd1003c8d451d523
Matthew Bolton <matthewwbolton@gmail.com>
2020-08-06 11:06:02 -0400

    -updated markov.py

--- applications/markov/markov.py
+++ applications/markov/markov.py
@@ -2,15 +2,29 @@ import random
 
 # Read in all the words in one go
 with open("input.txt") as f:
-    words = f.read().strip().split().replace("\n", " ")
-    print(words)
+    words = f.read().strip().replace("\n", " ").split(" ")
+    # print(words)
 
 # TODO: analyze which words can follow other words
 # Your code here
 
 d = dict()
 
-for word, index in enumerate(words):
+for index, word in enumerate(words):
+    if index < (len(words)-1):
+        if word == "":
+            word = " "
+        elif words[index + 1] == "":
+            d[word] = " "
+        else:
+            d[word] = words[index + 1]
+# print(d)
 
-    # TODO: construct 5 random sentences
-    # Your code here
+
+# TODO: construct 5 random sentences
+# Your code here
+
+def make_sentence():
+
+    try:
+        start = get_starting_word()
