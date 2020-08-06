36dc662a5fbab648318d75d81916a25d56e402b6
Shawn James <shawn.james@me.com>
2020-08-06 18:46:35 -0400

    MVP

--- applications/expensive_seq/expensive_seq.py
+++ applications/expensive_seq/expensive_seq.py
@@ -1,10 +1,16 @@
-# Your code here
 
-
-def expensive_seq(x, y, z):
-    # Your code here
-
-
+def expensive_seq(x, y, z, cache = dict()):
+    if x <= 0: #guard early exit
+        return y + z
+        
+    if (x, y, z) not in cache:
+        addend1 = expensive_seq(x - 1, y + 1, z, cache)
+        addend2 = expensive_seq(x - 2, y + 2, z * 2, cache)
+        addend3 = expensive_seq(x - 3, y + 3, z * 3, cache)
+        cache[(x, y, z)] = addend1 + addend2 + addend3
+        return cache[(x, y, z)]
+    else:
+        return cache[(x, y, z)]
 
 if __name__ == "__main__":
     for i in range(10):
