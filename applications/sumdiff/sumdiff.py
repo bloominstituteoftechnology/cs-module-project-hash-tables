41943ebb34572c43f4d22f3c3d225e0378bfdd61
Brandon Ramirez <bran.ramirez.don@gmail.com>
2020-08-05 20:19:53 -0700

    Implemented sumdiff function

--- applications/sumdiff/sumdiff.py
+++ applications/sumdiff/sumdiff.py
@@ -1,10 +1,16 @@
 """
 find all a, b, c, d in q such that
 f(a) + f(b) = f(c) - f(d)
+
+store results like so -> cache[f(a) + f(b)] = (a, b)
+have to check the other values -> 
+    z = f(c) - f(d)
+    if z in cache:
+        print(f"a: {cache[z][0]}, b: {cache[z][1]}, c: {c}, d: {d}")
 """
 
-#q = set(range(1, 10))
-#q = set(range(1, 200))
+# q = set(range(1, 10))
+# q = set(range(1, 200))
 q = (1, 3, 4, 7, 12)
 
 
@@ -13,3 +19,15 @@ def f(x):
 
 # Your code here
 
+
+def sumdiff(data):
+    for a in data:
+        for b in data:
+            for c in data:
+                for d in data:
+                    if f(a) + f(b) == f(c) - f(d):
+                        print(f"a: {a}, b: {b}, c: {c}, d: {d}")
+
+
+if __name__ == "__main__":
+    sumdiff(q)
