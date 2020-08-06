02f0fa6e292ecb4a2aba86a2adec933606712003
Michael Lowe <mrlowememphis@yahoo.com>
2020-08-06 04:35:03 -0500

    Day 1 rework using FNV

--- hashtable/hashtable.py
+++ hashtable/hashtable.py
@@ -6,146 +6,135 @@ class HashTableEntry:
         self.key = key
         self.value = value
         self.next = None
-
-
 # Hash table can't have fewer than this many slots
 MIN_CAPACITY = 8
-
-
 class HashTable:
     """
     A hash table that with `capacity` buckets
     that accepts string keys
-
     Implement this.
     """
 
     def __init__(self, capacity):
         # Your code here
-        self.capacity = capacity
-        self.hashTable = [None] * capacity
-        #added day 1
 
+        self.capacity = capacity
+        self.data = [None] * capacity
 
     def get_num_slots(self):
         """
         Return the length of the list you're using to hold the hash
         table data. (Not the number of items stored in the hash table,
         but the number of slots in the main list.)
-
         One of the tests relies on this.
-
         Implement this.
         """
         # Your code here
-        return self.capacity
 
+        return len(self.data)
 
     def get_load_factor(self):
         """
         Return the load factor for this hash table.
-
         Implement this.
         """
         # Your code here
-        # return self.count / self.capacity
-      
+        return len(self.data) * 0.75
 
 
     def fnv1(self, key):
         """
         FNV-1 Hash, 64-bit
-
         Implement this, and/or DJB2.
         """
 
         # Your code here
 
+        fnv_prime = 1099511628211
+        offset_basis =  14695981039346656037
+        hash_value = offset_basis
+        key_utf8 = key.encode()
+        for byte in key_utf8:
+            hash_value = hash_value ^ byte
+            hash_value = hash_value * fnv_prime
+        return hash_value
+
 
     def djb2(self, key):
         """
         DJB2 hash, 32-bit
-
         Implement this, and/or FNV-1.
         """
         # Your code here
-        hash = 5381
-        for x in key:
-            hash = ((hash << 5) + hash) + ord(x)
-        return hash
-        #added day 1
 
+        hash_value = 5381
+        for char in key:
+            hash_value = (hash_value * 33) + ord(char)
+        return hash_value
 
     def hash_index(self, key):
         """
         Take an arbitrary key and return a valid integer index
         between within the storage capacity of the hash table.
         """
-        #return self.fnv1(key) % self.capacity
-        return self.djb2(key) % self.capacity
+        return self.fnv1(key) % self.capacity
+        # return self.djb2(key) % self.capacity
 
     def put(self, key, value):
         """
         Store the value with the given key.
-
         Hash collisions should be handled with Linked List Chaining.
-
         Implement this.
         """
         # Your code here
-        new_entry = HashTableEntry(key, value)
-        idx = self.hash_index(key)
-        self.hashTable[idx] = new_entry
-        #added day 1
 
+        index = self.hash_index(key)
+        if self.data[index] is None:
+            self.data[index] = HashTableEntry(key, value)
+        else:
+            self.data[index].value = value  
 
     def delete(self, key):
         """
         Remove the value stored with the given key.
-
         Print a warning if the key is not found.
-
         Implement this.
         """
         # Your code here
-        idx = self.hash_index(key)
-        self.hashTable[idx].value = None
-        #added day 1
+        index = self.hash_index(key)
+        if self.data[index] is not None:
+            self.data[index].value = None
+        else:
+            print("Key was not found.")
 
 
     def get(self, key):
         """
         Retrieve the value stored with the given key.
-
         Returns None if the key is not found.
-
         Implement this.
         """
         # Your code here
-        idx = self.hash_index(key)
-        print('self', self.hashTable[idx])
-        return self.hashTable[idx].value
-        #added day 1
 
+        index = self.hash_index(key)
+        if self.data[index] is None:
+            return None
+        if self.data[index].key is key:
+            return self.data[index].value
 
     def resize(self, new_capacity):
         """
         Changes the capacity of the hash table and
         rehashes all key/value pairs.
-
         Implement this.
         """
         # Your code here
-        table = HashTable(10000)
-        print(table.put('hello', 'world'))
-        print(table.get('hello'))
-        #added day 1
 
 
+        pass
 
 if __name__ == "__main__":
     ht = HashTable(8)
-
     ht.put("line_1", "'Twas brillig, and the slithy toves")
     ht.put("line_2", "Did gyre and gimble in the wabe:")
     ht.put("line_3", "All mimsy were the borogoves,")
@@ -158,22 +147,22 @@ if __name__ == "__main__":
     ht.put("line_10", "Long time the manxome foe he sought--")
     ht.put("line_11", "So rested he by the Tumtum tree")
     ht.put("line_12", "And stood awhile in thought.")
-
     print("")
-
     # Test storing beyond capacity
     for i in range(1, 13):
         print(ht.get(f"line_{i}"))
-
     # Test resizing
     old_capacity = ht.get_num_slots()
     ht.resize(ht.capacity * 2)
     new_capacity = ht.get_num_slots()
-
     print(f"\nResized from {old_capacity} to {new_capacity}.\n")
-
     # Test if data intact after resizing
     for i in range(1, 13):
         print(ht.get(f"line_{i}"))
 
-    print("")
+   
+    print("") 
+
+
+
+
