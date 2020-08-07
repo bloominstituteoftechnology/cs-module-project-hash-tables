64628c3cb8fda01ab3652372836f1d24122866c8
Bharat Kumar <bharatiosdeveloper@gmail.com>
2020-08-06 17:06:26 -0700

    Implemented the letter count method

--- applications/word_count/word_count.py
+++ applications/word_count/word_count.py
@@ -1,9 +1,22 @@
+import re
 def word_count(s):
     # Your code here
+    special_characters = ['\"', ':', ';', ',', '.', '-', '+', '=', '/', '\\', '|', '[', ']', '{', '}', '(', ')', '*', '^', '&']
+    filtered_string = ''.join(filter(lambda c: c not in special_characters, s))
+    lower_string = filtered_string.lower()
+    array_word = lower_string.split()
 
+    dictionary = {}
 
+    for word in array_word:
+        if word not in dictionary:
+            dictionary[word] = 0
+            
+        dictionary[word] += 1
 
-if __name__ == "__main__":
+    return dictionary
+
+if __name__ == "__main__":    
     print(word_count(""))
     print(word_count("Hello"))
     print(word_count('Hello, my cat. And my cat doesn\'t say "hello" back.'))
