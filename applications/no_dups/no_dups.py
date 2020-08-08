2948e9cb30a58118a622821ce344c7dd627db33f
Michael Lowe <mrlowememphis@yahoo.com>
2020-08-08 09:03:38 -0500

    no_dups complete test running

--- applications/no_dups/no_dups.py
+++ applications/no_dups/no_dups.py
@@ -1,5 +1,14 @@
 def no_dups(s):
     # Your code here
+    words = s.split()
+    word_count = {}
+    unique_words = ""
+    for word in words:
+        if word_count.__contains__(word) is False:
+            word_count[word] = 1
+            unique_words += f"{word} "
+
+    return unique_words.rstrip()
 
 
 
