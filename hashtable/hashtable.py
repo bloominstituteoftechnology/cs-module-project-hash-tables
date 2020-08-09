b0c031d65b799bd989ea3b3fb6448c48825c79bf
chrishuskey <chrishuskey@users.noreply.github.com>
2020-08-08 20:44:02 -0700

    Add fully implemented HashTable class with collision avoidance via chaining. Passes all tests.

--- hashtable/hashtable.py
+++ hashtable/hashtable.py
@@ -1,6 +1,12 @@
 # Import libraries, packages, modules needed:
 import sys
 
+# Constants:
+HASH_TABLE_MIN_CAPACITY = 8
+LOAD_FACTOR_MAX = 0.70
+LOAD_FACTOR_MIN = 0.2
+RESIZE_FACTOR = 2
+
 
 class HashTableEntry:
     """
@@ -13,35 +19,86 @@ class HashTableEntry:
 
 
 class LinkedList:
+    """
+    Singly linked list.
+    """
     def __init__(self):
         self.head = None
         self.tail = None
+        self.length = 0
     
     def find(self, key):
-        pass
+        """
+        Find the given key if it exists in the LL, and return the node with that key (not only the value).
+        """
+        current = self.head
+        # Cycle through LL nodes to find one with the key we're looking for:
+        while current is not None:
+            if current.key == key:
+                return current
+            else:
+                current = current.next
+        # If not found, return None:
+        return None
 
-    def insert(self, key, value):
-        pass
+    def insert_or_update(self, key, value):
+        # Check if key is already in the LL:
+        existing_node = self.find(key=key)
+        # If it is already in the LL, then just update the value:
+        if existing_node is not None:
+            existing_node.value = value
+        # Otherwise if not in the existing LL, add the value to the LL (as new node at head of LL):
+        else:
+            # Create node for new value:
+            new_node = HashTableEntry(key=key, value=value)
+            # Insert new node at head of list:
+            new_node.next = self.head
+            # Update head of LL to new node, and update LL length:
+            self.head = new_node
+            self.length += 1
+            # If no existing tail (i.e., this is the only 1 node in this LL), set tail to new node too:
+            if self.tail is None:
+                self.tail = new_node
+
 
     def delete(self, key):
-        pass
-
-
-# Hash table can't have fewer than this many slots
-MIN_CAPACITY = 8
+        prev = None
+        current = self.head
+        # Cycle through LL nodes to find one with the key we're looking for:
+        while current is not None:
+            next = current.next
+            # If we find the key, remove that node from the LL (Python will then delete it from 
+            # memory later because no remaining references to the node):
+            if current.key == key:
+                # If node is the head of the LL, update the head:
+                if current is self.head:
+                    self.head = current.next
+                # If node is the tail of the LL, update the tail:
+                if current is self.tail:
+                    self.tail = prev
+                # Connect any previous node to any next node in the LL, skipping over the node we are removing:
+                if prev is not None:
+                    prev.next = next
+                current.next =  None
+                # Update the LL's length:
+                self.length -= 1
+                return True
+            else:
+                prev = current
+                current = current.next
+        # If not found, return False:
+        return False
 
 
 class HashTable:
     """
     A hash table with `capacity` number of buckets
     that accepts string keys.
-
-    Implement this.
     """
 
     def __init__(self, capacity):
-        self.capacity = capacity
-        self.array = [None for number in range(self.capacity)]
+        self.capacity = max(capacity, HASH_TABLE_MIN_CAPACITY)
+        self.array = [LinkedList() for slot in range(self.capacity)]
 
 
     def get_num_slots(self):
@@ -63,7 +120,8 @@ class HashTable:
 
         Implement this.
         """
-        # Your code here
+        full_slots = len(list(filter(lambda linkedlist: linkedlist.head is not None, self.array)))
+        return full_slots / self.capacity
 
 
     def fnv1(self, key):
@@ -90,9 +148,6 @@ class HashTable:
             hash_code *= fnv_prime
             hash_code = hash_code ^ byte
         
-        # Modulo the result to make sure it is an index within the existing array:
-        hash_code %= self.capacity
-        
         return hash_code
 
 
@@ -113,9 +168,6 @@ class HashTable:
         for byte in key_as_bytes:
             # Faster C-style alt. syntax for the below: hash = ((hash << 5) + hash) + char
             hash = hash * 33 + byte
-        
-        # Modulo the result to make sure it is an index within the existing array:
-        hash %= self.capacity
         
         return hash
 
@@ -138,9 +190,6 @@ class HashTable:
             # The ^ operator below is the bitwise XOR operator:
             hash = ((hash * 33) ^ byte)  # Q: Should we then % 128 (same as % 0x10000000) this to "keep it 32-bit" bc "python ints don't overflow"?
         
-        # Modulo the result to make sure it is an index within the existing array:
-        hash %= self.capacity
-        
         return hash
 
 
@@ -149,22 +198,28 @@ class HashTable:
         Take an arbitrary key and return a valid integer index
         between within the storage capacity of the hash table.
         """
-        #return self.fnv1(key)
-        return self.djb2(key)
+        hash_code = self.djb2(key)
+
+        # Modulo the result to make sure it is an index within the existing array:
+        return hash_code % self.capacity
 
 
     def put(self, key, value):
         """
         Store the value with the given key.
-
-        Hash collisions should be handled with Linked List Chaining.
-
-        Implement this.
+        Hash collisions handled with Linked List Chaining.
         """
         # Get index for this key from our hash function:
-        index = self.djb2(key)
-        # Put the value for this key at that index:
-        self.array[index] = value
+        index = self.hash_index(key)
+        # Get the LL at that index (even if it is empty, i.e., has no nodes):
+        LL = self.array[index]
+        # Check if this key is already in the LL at this index. If so, update it. 
+        # If not, add a node with this key:value pair to the LL at this index:
+        LL.insert_or_update(key=key, value=value)
+
+        # Resize if needed:
+        if self.get_load_factor() > LOAD_FACTOR_MAX:
+            self.resize(new_capacity = RESIZE_FACTOR * self.capacity)
 
 
     def delete(self, key):
@@ -176,9 +231,15 @@ class HashTable:
         Implement this.
         """
         # Get index for this key from our hash function:
-        index = self.djb2(key)
-        # "Delete" the value at that index (for the provided key) by setting it back to None:
-        self.array[index] = None
+        index = self.hash_index(key)
+        # Get the LL at that index (even if it is empty, i.e., has no nodes):
+        LL = self.array[index]
+        # Check if this key is in the LL at this index. If so, delete it.
+        LL.delete(key=key)
+
+        # Resize if needed:
+        if self.get_load_factor() < LOAD_FACTOR_MIN:
+            self.resize(new_capacity = self.capacity // RESIZE_FACTOR)
 
 
     def get(self, key):
@@ -190,22 +251,46 @@ class HashTable:
         Implement this.
         """
         # Get index for this key from our hash function:
-        index = self.djb2(key)
-        # Return the value at that index (the value corresponding to the provided key):
-        return self.array[index]
+        index = self.hash_index(key)
+        # Get the LL at that index (even if it is empty, i.e., has no nodes):
+        LL = self.array[index]
+        # Check if this key is in the LL at this index. 
+        node = LL.find(key=key)
+        # If it is (returned node is not None), return the associated value:
+        if node is not None:
+            return node.value
+        else:
+            return None
 
 
     def resize(self, new_capacity):
         """
         Changes the capacity of the hash table and
         rehashes all key/value pairs.
-
-        Implement this.
         """
-        # Your code here
+        # Create new array of the right size:
+        new_array = [LinkedList() for slot in range(new_capacity)]
+        # Update capacity here first, because it is used in hash_index() method:
+        self.capacity = new_capacity
+
+        # Re-hash every element in old array --> add at the resulting index of new array:
+        for num, linkedlist in enumerate(self.array):
+            current_node = linkedlist.head
+            # Add keys, values in the current LL to new array:
+            while current_node is not None:
+                # Get each key and value:
+                key, value = current_node.key, current_node.value
+                #  Hash the key to get insert index for new array --> get the LL at that index (even if LL has no nodes):
+                LL = new_array[self.hash_index(key)]
+                # Add key, value to the LL (at the right index in the new array):
+                LL.insert_or_update(key=key, value=value)
+                # Increment: Move to next node in LL:
+                current_node = current_node.next
+        # Update the hash table's underlying array to the new array:
+        self.array = new_array
 
 
-
+# # To test:
 # if __name__ == "__main__":
 #     ht = HashTable(8)
 
