29ee1e008f2b8ec2ee16d88a2719c3e380d8064e
Hui <jeffrey.x.hui@gmail.com>
2020-08-10 17:00:55 -0700

    implemented expseq

--- applications/expensive_seq/expensive_seq.py
+++ applications/expensive_seq/expensive_seq.py
@@ -1,10 +1,18 @@
 # Your code here
-
+cache = {}
 
 def expensive_seq(x, y, z):
-    # Your code here
-
-
+    if x <= 0: 
+        return y + z
+    if x >  0: 
+        if (x-2,y+2,z*2) not in cache:
+            cache[(x-1, y+1, z)] = expensive_seq(x-1,y+1,z)
+        if (x-2,y+2,z*2) not in cache:
+            cache[(x-2,y+2,z*2)] = expensive_seq(x-2,y+2,z*2)
+        if (x-3,y+3,z*3) not in cache:
+            cache[(x-3,y+3,z*3)] = expensive_seq(x-3,y+3,z*3)
+        
+        return cache[(x-1, y+1, z)] + cache[(x-2,y+2,z*2)] + cache[(x-3,y+3,z*3)]
 
 if __name__ == "__main__":
     for i in range(10):
