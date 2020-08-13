ac7a29a572bb09c448ecdf6bf2a82f815adbef5d
stevekutz <stkutz@gmail.com>
2020-08-13 17:55:47 -0500

    markov building sentences

--- applications/markov/markov.py
+++ applications/markov/markov.py
@@ -30,11 +30,26 @@ for index, word in enumerate(words):
         freq_dict[word].append(words[index + 1])
 
 # print(f' >>>>>>>>>> start_w :  {start_w}')
-for k,v in freq_dict.items():
-    print(f' key {k}: {v} \n')
+# for k,v in freq_dict.items():
+#     print(f' key {k}: {v} \n')
 
 
 #  print(freq_dict)
 # TODO: construct 5 random sentences
 # Your code here
 
+for line in range(10):
+
+    s = random.choice(start_w)
+    if s not in stop_w:
+        # loop up a random word in freq_dict
+        next_w = random.choice(freq_dict[s])
+
+        # check if not stop_w
+        while next_w not in stop_w:
+            s += " " + next_w
+            # try another word
+            next_w = random.choice(freq_dict[next_w])
+        s += " " + next_w
+    s += s + random.choice(end_punc)
+    print(f' Sentence: {s}')    
