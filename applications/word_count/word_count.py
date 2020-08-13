3cc31a5f86cde18f5e7941f63c07a75c3d0cb1b5
AlbertoGalvan001 <54903554+AlbertoGalvan001@users.noreply.github.com>
2020-08-13 18:43:06 -0400

    word_count complete

--- applications/word_count/word_count.py
+++ applications/word_count/word_count.py
@@ -1,6 +1,22 @@
 def word_count(s):
     # Your code here
+    string_count = {}
+    split_string = s.split()
+    temp_word = ''
+    banned_characters = '" : ; , . - + = / \ | [ ] { } ( ) * ^ &'
 
+    for index, word in enumerate(split_string):
+        temp_word = ''
+        for char in word:
+            if char not in banned_characters:
+                temp_word += char
+            if len(temp_word) > 0:
+                if temp_word.lower() in string_count:
+                    string_count[temp_word.lower()] += 1
+                else:
+                    string_count[temp_word.lower()] = 1
+
+    return string_count                      
 
 
 if __name__ == "__main__":
