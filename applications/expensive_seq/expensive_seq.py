90b44cd4475096a7d777898adf62f0eaa7f1a8d1
JimKing100 <JimKing100@users.noreply.github.com>
2020-08-27 07:06:19 -0700

    expensive sequence

--- applications/expensive_seq/expensive_seq.py
+++ applications/expensive_seq/expensive_seq.py
@@ -1,13 +1,18 @@
-# Your code here
+dict = {}
 
 
 def expensive_seq(x, y, z):
-    # Your code here
-
+    if x <= 0:
+        return y + z
+    elif not (x, y, z) in dict:
+        dict[(x, y, z)] = expensive_seq(x-1, y+1, z) + \
+                          expensive_seq(x-2, y+2, z*2) + \
+                          expensive_seq(x-3, y+3, z*3)
+    return dict[(x, y, z)]
 
 
 if __name__ == "__main__":
-    for i in range(10):
+    for i in range(15):
         x = expensive_seq(i*2, i*3, i*4)
         print(f"{i*2} {i*3} {i*4} = {x}")
 
