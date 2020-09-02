bb16964bcb889106f6c5c6069b09cec21938b186
JimKing100 <JimKing100@users.noreply.github.com>
2020-09-02 16:44:09 -0700

    histo markov crack_caesar

--- applications/histo/histo.py
+++ applications/histo/histo.py
@@ -1,2 +1,29 @@
-# Your code here
+with open("robin.txt") as f:
+    s = f.read()
 
+counts = dict()
+
+punc = ":;,.-+= /\\|[]{}()*^&\""
+for ele in s:
+    if ele in punc:
+        s = s.replace(ele, " ")
+
+words = s.lower().split()
+
+for word in words:
+    if word in counts:
+        counts[word] += 1
+    else:
+        counts[word] = 1
+
+sort_counts = sorted(counts.items(), key=lambda x: (x[1] * -1, x[0]))
+
+if sort_counts:
+    list_len = len(sort_counts)
+    for i in range(list_len):
+        word_len = len(sort_counts[i][0])
+        word_str = sort_counts[i][0] + (" " * (20 - word_len))
+        print(word_str, end=' ')
+        for i in range(sort_counts[i][1]):
+            print('', end='#')
+        print('')
--- applications/markov/markov.py
+++ applications/markov/markov.py
@@ -4,10 +4,44 @@ import random
 with open("input.txt") as f:
     words = f.read()
 
+words = ''.join([i for i in words if not i.isdigit()]).replace("\n", " ").split(' ')
+
 # TODO: analyze which words can follow other words
-# Your code here
+index = 1
+chain = {}
+
+for word in words[index:]:
+    key = words[index - 1]
+    if key in chain:
+        chain[key].append(word)
+    else:
+        chain[key] = [word]
+    index += 1
 
 
 # TODO: construct 5 random sentences
-# Your code here
 
+for i in range(5):
+    word1 = random.choice(list(chain.keys()))
+    while (word1[0].isupper() is False):
+        if word1[0] == '\"' and len(word1) > 1:
+            if word1[1].isupper() is True:
+               break
+        word1 = random.choice(list(chain.keys()))
+    message = word1
+
+    while (word1.endswith('.') is False) and \
+          (word1.endswith('?') is False) and \
+          (word1.endswith('!') is False):
+        if word1.endswith('\"') is True:
+            str_len = len(word1)
+            if (word1[str_len - 1] == '.') or \
+               (word1[str_len - 1] == '?') or \
+               (word1[str_len - 1] == '\"'):
+                break
+        word2 = random.choice(chain[word1])
+        word1 = word2
+        message += ' ' + word2
+
+    print(message)
+    print(' ')
