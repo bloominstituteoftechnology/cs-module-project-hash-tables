cae6c7cd6272953870708a9055cdcc7d31fc854c
Sal B Amer <iosdevsalman@gmail.com>
2020-09-01 18:56:44 -0700

    added  hashtables exmaple

---
+++ hashtables1.py
@@ -0,0 +1,14 @@
+#modulu 4  = [0,3]
+#modulo 5 = [0,4] - makes sure you are not out of bounds
+
+#get/store/delete functions
+
+# store(key="Key_3", value="value_3")
+# delete(key="Key_3")
+# hash function is scarambling input
+
+def my_hash(s):
+    for c in s:
+        print(c)
+
+my_hash("hello world")
