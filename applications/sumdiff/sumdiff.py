5800957eafaf78fb444ddf9494b8c358192dab1c
Skyler Suzuki <skylersuzuk1@gmail.com>
2020-09-03 15:25:07 -0700

    one solution for sumdiff. uses way too many loops.

--- applications/sumdiff/sumdiff.py
+++ applications/sumdiff/sumdiff.py
@@ -13,3 +13,35 @@ def f(x):
 
 # Your code here
 
+sums = {}
+diffs = {}
+
+def build_sum_lookup(q):
+
+    for a in range(len(q)):
+        for b in range(len(q)):
+            sum = f(q[a]) + f(q[b])
+            sums[(q[a], q[b])] = sum
+
+def build_diff_lookup(q):
+
+    for c in range(len(q)):
+        for d in range(len(q)):
+            if q[c] >= q[d]:
+                diff = f(q[c]) - f(q[d])
+                diffs[(q[c], q[d])] = diff
+
+build_sum_lookup(q)
+build_diff_lookup(q)
+
+def sumdiff(q):
+
+    for a in range(len(q)):
+        for b in range(len(q)):
+            for c in range(len(q)):
+                for d in range(len(q)):
+                    if q[c] >= q[d]:
+                        if sums[(q[a], q[b])] == diffs[(q[c], q[d])]:
+                            print((q[a], q[b]), (q[c], q[d]))
+
+sumdiff(q)
