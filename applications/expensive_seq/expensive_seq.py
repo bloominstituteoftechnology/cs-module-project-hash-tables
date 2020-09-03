91d0e3b764d61031ae5fa6036fd5ddcfdc83bf34
Lambda_School_Loaner_277 <lambda_school_loaner_277@Lambda-School-Loaner-277s-MacBook-Pro.local>
2020-09-03 12:16:23 -0400

    2 tests complete and passing

--- applications/expensive_seq/expensive_seq.py
+++ applications/expensive_seq/expensive_seq.py
@@ -1,14 +1,15 @@
-# Your code here
-
-
-def expensive_seq(x, y, z):
-    # Your code here
-
+def expensive_seq(x, y, z, cache={}):
+    if (x, y, z) not in cache:
+        if x <= 0:
+            cache[(x, y, z)] = y + z
+        else:
+            cache[(x, y, z)] = expensive_seq(x-1, y+1, z) + \
+                expensive_seq(x-2, y+2, z*2) + expensive_seq(x-3, y+3, z*3)
+    return cache[(x, y, z)]
 
 
 if __name__ == "__main__":
     for i in range(10):
         x = expensive_seq(i*2, i*3, i*4)
         print(f"{i*2} {i*3} {i*4} = {x}")
-
     print(expensive_seq(150, 400, 800))
