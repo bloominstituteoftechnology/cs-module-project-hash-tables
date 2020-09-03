bffeb8436b00ba800cdb788d15c78baffbee98e8
ferror18 <ferror18@gmail.com>
2020-09-03 13:16:33 -0700

    look up table

--- applications/lookup_table/lookup_table.py
+++ applications/lookup_table/lookup_table.py
@@ -1,5 +1,6 @@
+import random, math
 # Your code here
-
+cache = {}
 
 def slowfun_too_slow(x, y):
     v = math.pow(x, y)
@@ -15,11 +16,18 @@ def slowfun(x, y):
     output, but completes quickly instead of taking ages to run.
     """
     # Your code here
-
+    if (x,y) not in cache:
+        v = math.pow(x, y)
+        v = math.factorial(v)
+        v //= (x + y)
+        v %= 982451653
+        cache[(x,y)] = v
+        return v
+    else:
+        return cache[(x,y)]
 
 
 # Do not modify below this line!
-
 for i in range(50000):
     x = random.randrange(2, 14)
     y = random.randrange(3, 6)
