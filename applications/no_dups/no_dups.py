18278b5bd29eb34674f0e57463226c4135908384
Dan Anderson <danfanderson@gmail.com>
2020-09-08 22:14:51 -0500

    Update no_dups.py

--- applications/no_dups/no_dups.py
+++ applications/no_dups/no_dups.py
@@ -1,7 +1,23 @@
 def no_dups(s):
-    # Your code here
+    # Define a duplicate map
+    map_dupe = {}
 
+    # Split the string into words
+    arr_wrds = s.split()
+    ret_str  = ""
 
+    # Iterate through the list of words
+    for wrd in arr_wrds:
+        # Is the word a dupe?
+        if wrd not in map_dupe:
+            # Not a dupe; add the word to the map and the return string
+            map_dupe[wrd] = True
+            ret_str       = ret_str + " " + wrd
+            continue
+
+        # Word is a dupe... continue processing
+
+    return ret_str.strip()
 
 if __name__ == "__main__":
     print(no_dups(""))
