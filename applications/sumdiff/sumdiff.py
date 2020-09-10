4defe760223ef3bf3087f3e7dabcd2a617c9a6e8
David Wright <davidtwright89@gmail.com>
2020-09-10 03:12:13 -0400

    Implemented a first-pass solution

--- applications/sumdiff/sumdiff.py
+++ applications/sumdiff/sumdiff.py
@@ -12,4 +12,24 @@ def f(x):
     return x * 4 + 6
 
 # Your code here
+# a = c - d - b - 3
+# b = c - d - a - 3
+# c = a + b + d + 3
+# d = c - a - b - 3
 
+def equivalentSumsAndDiffs(q):
+    results = []
+    for a in q:
+        for b in q:
+            for c in q:
+                for d in q:
+                    if a == c - d - b - 3:
+                        results.append([a, b, c, d])
+    return results
+
+def printEquivalentSumsAndDiffs(q):
+    results = equivalentSumsAndDiffs(q)
+    for r in results:
+        print(f"f({r[0]}) + f({r[1]}) = f({r[2]}) - f({r[3]})    {f(r[0])} + {f(r[1])} = {f(r[2])} - {f(r[3])}")
+    
+printEquivalentSumsAndDiffs(q)
