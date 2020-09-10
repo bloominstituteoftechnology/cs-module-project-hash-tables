aa8f09db544cbfeb7fb0ea996659466c696771de
Joshua Rutkowski <54906829+joshrutkowski@users.noreply.github.com>
2020-09-10 19:06:21 -0400

    Finished lookup_table and crack_caeser

--- applications/lookup_table/lookup_table.py
+++ applications/lookup_table/lookup_table.py
@@ -1,4 +1,7 @@
+
 # Your code here
+import math
+import random
 
 
 def slowfun_too_slow(x, y):
@@ -9,16 +12,18 @@ def slowfun_too_slow(x, y):
 
     return v
 
-def slowfun(x, y):
-    """
-    Rewrite slowfun_too_slow() in here so that the program produces the same
-    output, but completes quickly instead of taking ages to run.
-    """
-    # Your code here
 
-
+def slowfun(x, y, cache={}):
+    if (x, y) not in cache:
+        v = math.pow(x, y)
+        v = math.factorial(v)
+        v //= (x + y)
+        v %= 982451653
+        cache[x, y] = v
+    return cache[(x, y)]
 
 # Do not modify below this line!
+
 
 for i in range(50000):
     x = random.randrange(2, 14)
