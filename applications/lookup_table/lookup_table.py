35460f8370e55ace18728c1c22c0325de0ec381d
jpeone <jesse.gene.peone@gmail.com>
2020-09-09 20:14:46 -0700

    i wanst storing modulo calculations, fixed

--- applications/lookup_table/lookup_table.py
+++ applications/lookup_table/lookup_table.py
@@ -9,6 +9,22 @@ power = HashTable()
 division = HashTable()
 modulo = HashTable()
 
+def part_fact(start, value, end):
+    for i in range(start + 1, end):
+        value *= i
+    return value
+
+def nearest_fact(value):
+    # because the test case won't generate less than 8 factorial
+    # a little specific an hacky, but fine for this example since we don't have
+    # unit tests to consider.
+    for i in range(int(value), 7, -1):
+        entry = factorial.get(str(i))
+        if entry:
+            return (i, entry)
+    return (None, None)
+
+
 
 def slowfun_too_slow(x, y):
     v = math.pow(x, y)
@@ -23,10 +39,14 @@ def slowfun(x, y):
     Rewrite slowfun_too_slow() in here so that the program produces the same
     output, but completes quickly instead of taking ages to run.
     """
+    global factorial, power, division, modulo
+
+
     p_key = f'{x}^{y}'
     entry = power.get(p_key)
 
     if entry:
+        print('found power')
         v = entry
     else:
         v = math.pow(x, y)
@@ -38,16 +58,25 @@ def slowfun(x, y):
     # TODO: Consider looking for the nearest factorial and computing from there
 
     if entry:
+        print('found factorial')
         v = entry
     else:
-        v = math.factorial(v)
-        factorial.put(f_key, v)
+        i, nearest = nearest_fact(v)
+        if i:
+            v = part_fact(1, nearest, v)
+            factorial.put(f_key, v)
+        else:
+            v = math.factorial(v)
+            factorial.put(f_key, v)
 
     denom = x + y
     d_key = f'{v}//{denom}'
     entry = division.get(d_key)
 
+    # TODO: Consider common denoms
+
     if entry:
+        print('found division')
         v = entry
     else:
         v //= denom
@@ -57,9 +86,12 @@ def slowfun(x, y):
     entry = modulo.get(m_key)
 
     if entry:
+        print('found modulo')
         return entry
     else:
-        return v % 982451653
+        v %= 982451653
+        modulo.put(m_key, v)
+        return v
 
 
 # Do not modify below this line!
