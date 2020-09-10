bada158728cad3997dd255dffcc8c14adaabda2c
Denis Cedeno <denis.cedeno@gmail.com>
2020-09-10 18:58:57 -0400

    expensive_seq

--- applications/expensive_seq/expensive_seq.py
+++ applications/expensive_seq/expensive_seq.py
@@ -3,10 +3,15 @@
 
 def expensive_seq(x, y, z):
     # Your code here
-exps(x, y, z) =
-     if x <= 0: y + z
-     if x >  0: exps(x-1,y+1,z) + exps(x-2,y+2,z*2) + exps(x-3,y+3,z*3)
-
+    #  ROT13 -> In Python, a dict key can be any immutable type... including a tuple.
+    hashTable = {}
+    inputTup = (x, y, z)
+    if inputTup not in hashTable:
+        if x <= 0: 
+            hashTable[inputTup] = y + z
+        if x >  0: 
+            hashTable[inputTup] = expensive_seq(x-1,y+1,z) + expensive_seq(x-2,y+2,z*2) + expensive_seq(x-3,y+3,z*3)
+    return hashTable[inputTup]
 
 if __name__ == "__main__":
     for i in range(10):
