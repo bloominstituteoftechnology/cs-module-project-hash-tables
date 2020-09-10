68c802b4a5ee3dd401a44470d3e2f2e19ae244c1
Richard Olson <8571182+richardOlson@users.noreply.github.com>
2020-09-09 22:13:16 -0600

    sumdiff

--- try.py
+++ try.py
@@ -14,3 +14,27 @@ theTuple = (".", "\"")
 
 print(f"first is {first.endswith(theTuple)}")
 print(f"second is  {second.endswith(theTuple)}")
+
+
+
+
+theList = [1,2,3,4]
+
+def myPerm(theList):
+    outPut = []
+    if len(theList) == 1:
+        return theList
+  
+    for i in range(len(theList)):
+        # doing the swapping of the 
+        # value that was chosen
+        
+        # will now do the recursive call to the permutaton function
+        
+        outPut += [theList[i]] + myPerm(theList[:i] + theList[i+1:])
+    return outPut
+
+
+print(myPerm(theList))
+
+
