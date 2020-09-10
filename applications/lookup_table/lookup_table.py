152f1c4b29d8c435cb52142fb897c06fd6a8e477
jpeone <jesse.gene.peone@gmail.com>
2020-09-09 19:25:37 -0700

    naive implementation to the lookup problem

--- applications/lookup_table/lookup_table.py
+++ applications/lookup_table/lookup_table.py
@@ -1,4 +1,13 @@
 # Your code here
+import math
+import random
+from hashtable import HashTable
+
+
+factorial = HashTable()
+power = HashTable()
+division = HashTable()
+modulo = HashTable()
 
 
 def slowfun_too_slow(x, y):
@@ -14,8 +23,43 @@ def slowfun(x, y):
     Rewrite slowfun_too_slow() in here so that the program produces the same
     output, but completes quickly instead of taking ages to run.
     """
-    # Your code here
+    p_key = f'{x}^{y}'
+    entry = power.get(p_key)
 
+    if entry:
+        v = entry
+    else:
+        v = math.pow(x, y)
+        power.put(p_key, v)
+
+    f_key = f'{v}'
+    entry = factorial.get(f_key)
+
+    # TODO: Consider looking for the nearest factorial and computing from there
+
+    if entry:
+        v = entry
+    else:
+        v = math.factorial(v)
+        factorial.put(f_key, v)
+
+    denom = x + y
+    d_key = f'{v}//{denom}'
+    entry = division.get(d_key)
+
+    if entry:
+        v = entry
+    else:
+        v //= denom
+        division.put(d_key, v)
+
+    m_key = f'{v}'
+    entry = modulo.get(m_key)
+
+    if entry:
+        return entry
+    else:
+        return v % 982451653
 
 
 # Do not modify below this line!
