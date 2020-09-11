9116e3e212f64e0e9078577385de212b0677915f
John Kouris <jdkouris@gmail.com>
2020-09-10 18:55:26 -0700

    Finished word count and expensive sequence

--- applications/word_count/word_count.py
+++ applications/word_count/word_count.py
@@ -1,6 +1,19 @@
 def word_count(s):
     # Your code here
+    word_dict = {}
 
+    special_chars = ['"', ':', ';', ',', '.', '-', '+', '=', '/', '\\', '|', '[', ']', '{', '}', '(', ')', '*', '^', '&']
+    s2 = ''.join(c.lower() for c in s if not c in special_chars)
+    word_list = s2.split()
+
+    for word in word_list:
+        if word == " ":
+            continue
+        elif word in word_dict:
+            word_dict[word] += 1
+        else:
+            word_dict[word] = 1
+    return word_dict
 
 
 if __name__ == "__main__":
