6ee3dedcb945bb1c9d0683b4a1d22207b4bad372
Jose <pjose14.jp38@yahoo.com>
2020-09-13 10:01:54 -0500

    just a small add

--- applications/expensive_seq/expensive_seq.py
+++ applications/expensive_seq/expensive_seq.py
@@ -1,9 +1,20 @@
 # Your code here
-
+store = {}
 
 def expensive_seq(x, y, z):
     # Your code here
-
+    key = (x, y, z)
+    if store.get(key) is not None:
+        return store[key]
+    
+    if x <= 0:
+        return y + z
+    
+    
+    store[key] = expensive_seq(
+        x-1, y+1, z) + expensive_seq(x-2, y+2, z*2) + expensive_seq(x-3, y+3, z*3)
+    
+    return store[key]
 
 
 if __name__ == "__main__":
