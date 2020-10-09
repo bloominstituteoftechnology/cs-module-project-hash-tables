d3445232ff882c496d8ceefbd3404f5761844793
Ariunbold <ariun1118@gmail.com>
2020-10-08 20:29:58 -0400

    finished all of them

--- applications/crack_caesar/crack_caesar.py
+++ applications/crack_caesar/crack_caesar.py
@@ -2,4 +2,56 @@
 # decode it.
 
 # Your code here
+import os.path
 
+input_txt = os.path.join(os.path.dirname(__file__), 'ciphertext.txt')
+
+# with open(input_txt) as f:
+#     cipherText = f.read()
+
+# def letterCount(s):
+#     dict = {}
+#     letters = [l.upper() for l in s if l.isalpha()]
+#     for letter in letters:
+#         dict[letter] = dict[letter] + 1 if letter in dict else 1
+#     return dict
+
+# def caesarCipherKey(cipherText):
+#     letters = ['E', 'T', 'A', 'O', 'H', 'N', 'R', 'I', 'S', 'D', 'L', 'W', 'U',
+#                'G', 'F', 'B', 'M', 'Y', 'C', 'P', 'K', 'V', 'Q', 'J', 'X', 'Z']
+#     cipherLettersSorted = sorted(letterCount(cipherText).items(), key=lambda x: x[1], reverse=True)
+#     key = {}
+#     for (i, l) in enumerate(cipherLettersSorted):
+#         key[l[0]] = letters[i]
+#     return key
+
+# def crackCaesarCipher(cipherText):
+#     key = caesarCipherKey(cipherText)
+#     decodedText = ""
+#     for c in cipherText:
+#         decodedText += key[c] if c in key else c
+#     print(decodedText)
+
+# crackCaesarCipher(cipherText)
+
+with open(input_txt) as f:
+    text = f.read()
+    text = text.replace('\n', ' ').replace('\\', '')
+    ht = {}
+    count = 0
+    for char in text:
+        if ord(char) >= 65 and ord(char) <= 90:
+            if char not in ht:
+                ht[char] = 1
+            else:
+                ht[char] += 1
+            count += 1
+    frequent_list = ['E', 'T', 'A', 'O', 'H', 'N', 'R', 'I', 'S', 'D', 'L', 'W', 'U',
+                     'G', 'F', 'B', 'M', 'Y', 'C', 'P', 'K', 'V', 'Q', 'J', 'X', 'Z']
+    ht = {k[0]: frequent_list[i] for i, k in enumerate(
+        sorted(ht.items(), key=lambda item: item[1], reverse=True))}
+    for char in text:
+        if ord(char) >= 65 and ord(char) <= 90:
+            print(ht[char], end="")
+        else:
+            print(char, end="")
--- applications/sumdiff/sumdiff.py
+++ applications/sumdiff/sumdiff.py
@@ -3,8 +3,8 @@ find all a, b, c, d in q such that
 f(a) + f(b) = f(c) - f(d)
 """
 
-#q = set(range(1, 10))
-#q = set(range(1, 200))
+q = set(range(1, 10))
+q = set(range(1, 200))
 q = (1, 3, 4, 7, 12)
 
 
@@ -12,4 +12,19 @@ def f(x):
     return x * 4 + 6
 
 # Your code here
+def equivalentSumsAndDiffs(q):
+    solutionSet = set()
+    for a in q:
+        for b in q:
+            for c in q:
+                for d in q:
+                    if a == c - d - b - 3:
+                        solutionSet.add((a, b, c, d))
+    return list(solutionSet)
 
+def printEquivalentSumsAndDiffs(q):
+    results = equivalentSumsAndDiffs(q)
+    for r in results:
+        print(f"f({r[0]}) + f({r[1]}) = f({r[2]}) - f({r[3]})    {f(r[0])} + {f(r[1])} = {f(r[2])} - {f(r[3])}")
+
+printEquivalentSumsAndDiffs(q)
