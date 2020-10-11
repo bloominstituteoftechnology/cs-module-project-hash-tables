8d7c8f2be579bff849bd76b762a204dee0ec1d1c
vy3191 <venkyafhs@gmail.com>
2020-10-11 17:21:22 -0400

    Completed word_count assignment

--- applications/word_count/word_count.py
+++ applications/word_count/word_count.py
@@ -1,6 +1,23 @@
+import re
 def word_count(s):
     # Your code here
-
+    if s == '':
+        return {}
+    count_dict = {}
+    str_list = s.split()
+    # str_list = re.split('|\t|\n|\r', s)
+    for word in str_list:
+        single_word = word.lower()
+        # This below like remove the special characters from the word.
+        special_characters = r"""!"#$%&"()*+,-./:;<=>?@[\]^_`{|}~"""
+        trimmed_word = ''.join(character for character in single_word if character not in special_characters)
+        if trimmed_word not in count_dict:
+            if len(trimmed_word) >= 1:
+                count_dict[trimmed_word] = 1
+        else:
+            count_dict[trimmed_word] += 1    
+    print(count_dict)
+    return count_dict
 
 
 if __name__ == "__main__":
