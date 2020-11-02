457b16cf34f645ac06df4308c2a5f5f96b1f1b26
Matt Martindale <martindalematt18@yahoo.com>
2020-11-01 20:31:02 -0700

    Added delete and check_and_resize methods

--- hashtable/hashtable.py
+++ hashtable/hashtable.py
@@ -1,6 +1,7 @@
 class Node:
-  def __init__(self, value):
+  def __init__(self, value, key):
     self.value = value
+    self.key = key
     self.next = None
 
 class LinkedList:
@@ -27,11 +28,11 @@ class LinkedList:
 
     return None
 
-  def delete(self, value):
+  def delete(self, key):
     curr = self.head
 
     # special case for remove head
-    if curr.value == value:
+    if curr.key == key:
       self.head = curr.next
       curr.next = None
       return curr
@@ -39,7 +40,7 @@ class LinkedList:
     prev = None
 
     while curr != None:
-      if curr.value == value:
+      if curr.key == key:
         prev.next = curr.next
         curr.next = None
         return curr
@@ -127,76 +128,48 @@ class HashTable:
             # set table at index to newly created LL
             self.table[index] = ll
             # add created node to the head of the LL
-            ll.add_to_head(Node(value))
+            ll.add_to_head(Node(value, key))
             # increase count
             self.count += 1
+            self.check_and_resize()
         else:
             # if table at index already has a LL, get reference to current LL
             curr_ll: LinkedList = self.table[index]
             # set the new Node as the head
-            curr_ll.add_to_head(Node(value))
+            curr_ll.add_to_head(Node(value, key))
             # increase count
             self.count += 1
+            self.check_and_resize()
 
         
 
 
     def delete(self, key):
-        """
-        Remove the value stored with the given key.
 
-        Print a warning if the key is not found.
-
-        Implement this.
-        """
-        # Your code here
-        # delete_at_index = self.hash_index(key)
-
-        # if self.table[delete_at_index] is None:
-        #     print("No key found")
-        #     return
-
-        # self.table[delete_at_index] = None
-        # self.count -= 1
-
-        value = self.table[self.hash_index(key)]
-        if value == None:
-            print("No value for index")
-        self.table[self.hash_index(key)] = None
+        # value = self.table[self.hash_index(key)]
+        # if value == None:
+        #     print("No value for index")
+        # self.table[self.hash_index(key)] = None
+        index = self.hash_index(key)
+        ll: LinkedList = self.table[index]
+        ll.delete(key)
+        self.check_and_resize()
 
 
     def get(self, key):
-        """
-        Retrieve the value stored with the given key.
-
-        Returns None if the key is not found.
-
-        Implement this.
-        """
-        # Your code here
-        # index = self.hash_index(key)
 
         return self.table[self.hash_index(key)]
 
 
     def resize(self, new_capacity):
-        """
-        Changes the capacity of the hash table and
-        rehashes all key/value pairs.
+        old_table = self.table
+        self.table = [None] * new_capacity
 
-        Implement this.
-        """
-        # Your code here
-        # old_table = self.table
-        # self.table = [None] * new_capacity
-        # self.capacity = new_capacity
-
-        # for entry in old_table:
-        #     while entry is not None:
-        #         old_key = entry.key
-        #         old_value = entry.value
-        #         index = self.hash_index(old_key)
-        #         self.table[index] = HashTableEntry(old_key, old_value)
+    def check_and_resize(self):
+        if self.get_load_factor() > 0.7:
+            self.resize(self.capacity * 2)
+        elif self.get_load_factor() < 0.2:
+            self.resize(self.capacity / 2)
 
 
 
@@ -222,15 +195,21 @@ if __name__ == "__main__":
     for i in range(1, 13):
         print(ht.get(f"line_{i}"))
 
-    # Test resizing
-    old_capacity = ht.get_num_slots()
-    ht.resize(ht.capacity * 2)
-    new_capacity = ht.get_num_slots()
+    ht.delete("line_1")
 
-    print(f"\nResized from {old_capacity} to {new_capacity}.\n")
-
-    # Test if data intact after resizing
+    print("deleted")
     for i in range(1, 13):
         print(ht.get(f"line_{i}"))
 
-    print("")
+    # Test resizing
+    # old_capacity = ht.get_num_slots()
+    # ht.resize(ht.capacity * 2)
+    # new_capacity = ht.get_num_slots()
+
+    # print(f"\nResized from {old_capacity} to {new_capacity}.\n")
+
+    # # Test if data intact after resizing
+    # for i in range(1, 13):
+    #     print(ht.get(f"line_{i}"))
+
+    # print("")
