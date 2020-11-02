b5b823419186ed3e52ed7099a01d34ccc01ed5b5
Anthony <chitowncoder@gmail.com>
2020-11-01 23:31:57 -0600

    hashtable complete

--- hashtable/hashtable.py
+++ hashtable/hashtable.py
@@ -8,6 +8,58 @@ class HashTableEntry:
         self.next = None
 
 
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    def __repr__(self):
+        current_str = ""
+        current = self.head
+        while current is not None:
+            current_str += f"{str(current.value)} -> "
+            current = current.next
+        return current_str
+
+    def add_to_head(self, node):
+        node.next = self.head
+        self.head = node
+
+    def add_to_head_or_overwrite(self, node):
+        existing_node = self.find(node.key)
+        if existing_node is not None:
+            existing_node.key = node.key
+        else:
+            self.add_to_head(node)
+
+    def find(self, key):
+        current = self.head
+        while current is not None:
+            if current.key == key:
+                return current.value
+            current = current.next
+        return None
+
+    def delete(self, key):
+        current = self.head
+
+        if current.key == key:
+            self.head = current.next
+
+        prev = current
+        current = current.next
+
+        while current is not None:
+            if current.key == key:
+                prev.next = current.next
+                current.next = None
+                return current.value
+            else:
+                prev = current
+                current = current.next
+
+        return None
+
+
 # Hash table can't have fewer than this many slots
 MIN_CAPACITY = 8
 
@@ -16,23 +68,27 @@ class HashTable:
     """
     A hash table that with `capacity` buckets
     that accepts string keys
-
     Implement this.
     """
 
     def __init__(self, capacity):
-       self.capacity = MIN_CAPACITY
-       self.table = [None] * self.capacity
-       self.count = 0
+        # Your code here
+        self.capacity = capacity
+        self.table = [None] * capacity
+
+        for i in range(0, len(self.table)):
+            self.table[i] = LinkedList()
+
 
     def get_num_slots(self):
         """
         Return the length of the list you're using to hold the hash
         table data. (Not the number of items stored in the hash table,
         but the number of slots in the main list.)
-
-        One of the tests relies on this. if froggy do tonight 🤣
-
+        One of the tests relies on this.
+        
+         One of the tests relies on this. if froggy do tonight 🤣
+        
         Implement this.
         """
         # Your code here
@@ -42,33 +98,51 @@ class HashTable:
     def get_load_factor(self):
         """
         Return the load factor for this hash table.
-
         Implement this.
         """
         # Your code here
+        count = 0
+
+        for i in range(0, len(self.table)):
+            current = self.table[i].head
+            while current is not None:
+                count += 1
+                current = current.next
+        
+        load_factor = count / self.get_num_slots()
+        
+        if load_factor > 0.7:
+            self.resize(self.capacity * 2)
+        elif load_factor < 0.2:
+            if self.capacity // 2 < 8:
+                self.resize(8)
+            else:
+                self.resize(self.capacity // 2)
+        
+        return load_factor
 
 
     def fnv1(self, key):
         """
         FNV-1 Hash, 64-bit
-
         Implement this, and/or DJB2.
         """
 
         # Your code here
+        pass
 
 
     def djb2(self, key):
         """
         DJB2 hash, 32-bit
-
         Implement this, and/or FNV-1.
         """
         # Your code here
         hash = 5381
-        for x in key:
-            hash = ((hash << 5) + hash) + ord(x)
-        return hash & 0xffffffff
+        for c in key:
+            hash = (hash * 33) + ord(c)
+        return hash
+
 
     def hash_index(self, key):
         """
@@ -78,71 +152,60 @@ class HashTable:
         #return self.fnv1(key) % self.capacity
         return self.djb2(key) % self.capacity
 
+
     def put(self, key, value):
         """
         Store the value with the given key.
-
         Hash collisions should be handled with Linked List Chaining.
-
         Implement this.
         """
         # Your code here
-        index = self.hash_index(key)
-        #storing linked list with
-        node = HashTableEntry(key, value)
-        #print(node)
-        key = self.table[index]
-        self.count += 1
-        
-        #does it exist
-        if key:
-            self.table[index] = node
-            self.table[index].next = key
-        else:
-            self.table[index] = node
-        print(self.table)
-        return self.table
+        new_entry = HashTableEntry(key, value)
+        self.table[self.hash_index(key)].add_to_head(new_entry)
+
 
     def delete(self, key):
         """
         Remove the value stored with the given key.
-
         Print a warning if the key is not found.
-
         Implement this.
         """
         # Your code here
-        self.count -= 1
-        self.put(key, None)
+        return self.table[self.hash_index(key)].delete(key)
 
 
     def get(self, key):
         """
         Retrieve the value stored with the given key.
-
         Returns None if the key is not found.
-
         Implement this.
         """
         # Your code here
-        item = self.hash_index(key)
-        storage = self.table[item]
-        
-        while storage:
-            if storage.key == key:
-                return storage.value
-            storage = storage.next
+        if self.table[self.hash_index(key)].head is not None:
+            return self.table[self.hash_index(key)].find(key)
+        else:
+            return None
 
 
     def resize(self, new_capacity):
         """
         Changes the capacity of the hash table and
         rehashes all key/value pairs.
-
         Implement this.
         """
         # Your code here
+        old_capacity = self.capacity
+        self.capacity = new_capacity
 
+        for i in range(old_capacity, new_capacity):
+            self.table.append(None)
+            self.table[i] = LinkedList()
+
+        for i in range(0, old_capacity):
+            current_node = self.table[i].head
+            while current_node is not None:
+                self.put(current_node.key, current_node.value)
+                current_node = current_node.next        
 
 
 if __name__ == "__main__":
@@ -174,7 +237,7 @@ if __name__ == "__main__":
 
     print(f"\nResized from {old_capacity} to {new_capacity}.\n")
 
-    # Test if data intact after resizing
+    # # Test if data intact after resizing
     for i in range(1, 13):
         print(ht.get(f"line_{i}"))
 
