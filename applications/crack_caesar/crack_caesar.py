8e77ba31075268af3263e66aff5189b42f4ccb7f
Matt Martindale <martindalematt18@yahoo.com>
2020-11-04 16:58:16 -0700

    Cracked the cypher code!

--- applications/crack_caesar/crack_caesar.py
+++ applications/crack_caesar/crack_caesar.py
@@ -9,6 +9,7 @@ with open(input_text) as f:
 lowercase_words = words.lower()
 encrypted_dict = {}
 
+# Get frequency of characters in encryption
 for char in lowercase_words:
   if char.isalpha() != True:
     continue
@@ -17,4 +18,21 @@ for char in lowercase_words:
   else:
     encrypted_dict[char] = 1
 
+# Sort characters by frequency
 sorted_encrypted_dict = {k: v for k, v in sorted(encrypted_dict.items(), key=lambda item: item[1], reverse=True)}
+
+# Use sorted encryption dictionary to replace characters
+encryption_key = ['e', 't', 'a', 'o', 'h', 'n', 'r', 'i', 's', 'd', 'l', 'w', 'u', 'g', 'f', 'b', 'm', 'y', 'c', 'p', 'k', 'v', 'q', 'j', 'x', 'z']
+transfer_dict = {}
+result = ""
+
+for index, key in enumerate(sorted_encrypted_dict):
+  transfer_dict[key] = encryption_key[index]
+
+for char in lowercase_words:
+  if char.isalpha() != True:
+    result += char
+  if char in transfer_dict:
+    result += transfer_dict[char]
+
+print(result)
