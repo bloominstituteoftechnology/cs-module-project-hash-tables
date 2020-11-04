8c91cb0ba0ce60d9a1563600610bca3feab119af
Kelson Hartle <kelson99hartle@icloud.com>
2020-11-03 18:56:49 -0700

    fixed error in markov

--- applications/markov/markov.py
+++ applications/markov/markov.py
@@ -31,14 +31,15 @@ for word in words_split:
         # listy.append(word)
         # check to see if the word already exists
         if dictionary.get(word, '00') != '00':
+            dictionary[listy[0]].append(word)
             listy[0] = word
             does_Exist = True
-            # dictionary[word] = [word]
-            # dictionary[word].append(
         else:
             if does_Exist:
                 does_Exist = False
                 dictionary[listy[0]].append(word)
+                dictionary[word] = []
+                listy[0] = word
             
             else: 
                 # if it doesn't exist create an entry in the dict
@@ -51,20 +52,20 @@ for word in words_split:
 # print(dictionary["that"])
 # print(dictionary["this"])
 # TODO: construct 5 random sentences
-for keys in dictionary.keys():
-    print(keys)
+# for keys in dictionary.keys():
+#     print(keys)
 
-# current = dictionary["it"]
-# new_sentence = ""
-# counting = 0
-# while current:
-#     counting += 1
-#     random_selection = random.choice(current)
-#     new_sentence += random_selection
-#     new_sentence += " "
-#     current = dictionary[random_selection]
+current = dictionary["it"]
+new_sentence = ""
+counting = 0
+while current:
+    counting += 1
+    random_selection = random.choice(current)
+    new_sentence += random_selection
+    new_sentence += " "
+    current = dictionary[random_selection]
     
-#     if counting > 10:
-#         break
+    if counting > 30:
+        break
 
-# print(new_sentence)
+print(new_sentence)
