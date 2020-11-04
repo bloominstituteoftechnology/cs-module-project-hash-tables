9d0c10255a4f58aadcd7d02f5c3c563fb3de5ff5
Kelson Hartle <kelson99hartle@icloud.com>
2020-11-03 18:34:35 -0700

    markov working

--- applications/markov/markov.py
+++ applications/markov/markov.py
@@ -6,8 +6,65 @@ with open("input.txt") as f:
 
 # TODO: analyze which words can follow other words
 # Your code here
+forbidden = '":;,.-+=/\|[]{}()*^&"'
+ok = 0
+new_string = ""
+for word in words:
+    if word in forbidden:
+        ok = 0
+    else:
+        new_string += word.lower()
 
 
+words_split = new_string.split()
+
+is_first = True
+does_Exist = False
+dictionary = {}
+listy = []
+for word in words_split:
+    if is_first:
+        is_first = False
+        listy.append(word)
+        dictionary[word] = []
+    else:
+        # listy.append(word)
+        # check to see if the word already exists
+        if dictionary.get(word, '00') != '00':
+            listy[0] = word
+            does_Exist = True
+            # dictionary[word] = [word]
+            # dictionary[word].append(
+        else:
+            if does_Exist:
+                does_Exist = False
+                dictionary[listy[0]].append(word)
+            
+            else: 
+                # if it doesn't exist create an entry in the dict
+                dictionary[listy[0]] = [word]
+                dictionary[word] = []
+            
+                # set the first index to current word
+                listy[0] = word
+
+# print(dictionary["that"])
+# print(dictionary["this"])
 # TODO: construct 5 random sentences
-# Your code here
+for keys in dictionary.keys():
+    print(keys)
 
+# current = dictionary["it"]
+# new_sentence = ""
+# counting = 0
+# while current:
+#     counting += 1
+#     random_selection = random.choice(current)
+#     new_sentence += random_selection
+#     new_sentence += " "
+#     current = dictionary[random_selection]
+    
+#     if counting > 10:
+#         break
+
+# print(new_sentence)
