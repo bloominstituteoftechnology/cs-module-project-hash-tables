647d6495bb926ea3515b8e71c5c349b8d8390836
Matt Martindale <martindalematt18@yahoo.com>
2020-11-03 20:56:19 -0700

    got project to read input.txt file

--- applications/markov/markov.py
+++ applications/markov/markov.py
@@ -1,9 +1,13 @@
 import random
+import os.path
+
+input_text = os.path.join(os.path.dirname(__file__), 'input.txt')
 
 # Read in all the words in one go
-with open("input.txt") as f:
+with open(input_text) as f:
     words = f.read()
 
+print(words)
 # TODO: analyze which words can follow other words
 # Your code here
 
