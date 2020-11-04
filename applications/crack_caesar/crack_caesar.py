df14b518b4b2531892875fed1af451b4a82d9405
Matt Martindale <martindalematt18@yahoo.com>
2020-11-04 16:42:39 -0700

    Sorted encrypted data by frequency of character

--- applications/crack_caesar/crack_caesar.py
+++ applications/crack_caesar/crack_caesar.py
@@ -1,5 +1,20 @@
-# Use frequency analysis to find the key to ciphertext.txt, and then
-# decode it.
+import os
 
-# Your code here
+input_text = os.path.join(os.path.dirname(__file__), 'cyphertext.txt')
 
+# Read in all the words in one go
+with open(input_text) as f:
+    words = f.read()
+
+lowercase_words = words.lower()
+encrypted_dict = {}
+
+for char in lowercase_words:
+  if char.isalpha() != True:
+    continue
+  if char in encrypted_dict:
+    encrypted_dict[char] += 1
+  else:
+    encrypted_dict[char] = 1
+
+sorted_encrypted_dict = {k: v for k, v in sorted(encrypted_dict.items(), key=lambda item: item[1], reverse=True)}
