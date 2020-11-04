4c8ed072455d2ab4e7f99bd0173dd4f6f13dd6c0
Matt Martindale <martindalematt18@yahoo.com>
2020-11-04 09:56:16 -0700

    Created dictionary that contains words that can follow other words

--- applications/markov/markov.py
+++ applications/markov/markov.py
@@ -7,11 +7,19 @@ input_text = os.path.join(os.path.dirname(__file__), 'input.txt')
 with open(input_text) as f:
     words = f.read()
 
-print(words)
+# print(words)
 # TODO: analyze which words can follow other words
-# Your code here
+split_words = words.split()
+word_dictionary = {}
+
+for index, word in enumerate(split_words):
+  if word not in word_dictionary and index != len(split_words) - 1:
+    word_dictionary[word] = split_words[index + 1] + " "
+  elif word in word_dictionary:
+    word_dictionary[word] += split_words[index + 1] + " "
+
+print(word_dictionary)
 
 
 # TODO: construct 5 random sentences
 # Your code here
-
