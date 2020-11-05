517e9ae9c91e63c0a2df0f53f7fb4a3a281f360f
CarlosETurcios <turcios50@gmail.com>
2020-11-04 20:38:53 -0800

    finished hash table

--- hashtable/hashtable.py
+++ hashtable/hashtable.py
@@ -2,6 +2,7 @@ class HashTableEntry:
     """
     Linked List hash table key/value pair
     """
+
     def __init__(self, key, value):
         self.key = key
         self.value = value
@@ -26,11 +27,6 @@ class HashTable:
         self.load = 0
         self.table = [None] * self.capacity
 
-        
-
-
-
-
     def get_num_slots(self):
         """
         Return the length of the list you're using to hold the hash
@@ -44,8 +40,6 @@ class HashTable:
         # Your code here
         return self.capacity
 
-
-
     def get_load_factor(self):
         """
         Return the load factor for this hash table.
@@ -53,8 +47,10 @@ class HashTable:
         Implement this.
         """
         # Your code here
-        # dont do yet 
-
+        # dont do yet
+        # number of items divided by number of buckets
+        # try to keep between 20% and 70%
+        return self.load / self.capacity
 
     def fnv1(self, key):
         """
@@ -66,25 +62,19 @@ class HashTable:
         # Your code here
         # what is a FNV-1 Hash, 64 bit?
         # Non-crytographic hash function created by Glenn Fowler, London Curt Noll, and Kiem-Phong Vo.
-        # Constatnts 
-        # Start with an initail hash value 
+        # Constatnts
+        # Start with an initail hash value
         FNV_prime = 1099511628211
         offset_basis = 14695981039346656037
         key_of_data_bytes = key.encode()
 
-        hash = offset_basis 
+        hash = offset_basis
         for letter in key_of_data_bytes:
             hash = hash * FNV_prime
             # Use the XOR operator ^ between two values to perform bitwise "exclusive or" on their binary representations. When used between two integers, the XOR operator returns an integer.
-            hash = hash^letter
-            
+            hash = hash ^ letter
+
         return hash
-     
-
-
-        
-
-
 
     def djb2(self, key):
         """
@@ -93,8 +83,6 @@ class HashTable:
         Implement this, and/or FNV-1.
         """
         # Your code here
-      
-
 
     def hash_index(self, key):
         """
@@ -102,7 +90,7 @@ class HashTable:
         between within the storage capacity of the hash table.
         """
         return self.fnv1(key) % self.capacity
-        #return self.djb2(key) % self.capacity
+        # return self.djb2(key) % self.capacity
 
     def put(self, key, value):
         """
@@ -113,18 +101,24 @@ class HashTable:
         Implement this.
         """
         # Your code here
+        # hashed_string = self.hash_index(key)
+
+        # if self.table[hashed_string] != None:
+        #     print("warning collistion!!!")
+
+        # self.table[hashed_string] = value
+
+        # self.load += 1
         hashed_string = self.hash_index(key)
+        new_node = HashTableEntry(key, value)
+        curr = self.table[hashed_string]
 
-        if self.table[hashed_string] != None:
-            print("warning collistion!!!")
-        
-        self.table[hashed_string] = value
-
+        if self.table is not None:
+            self.table[hashed_string] = new_node
+            self.table[hashed_string].next = curr
+        else:
+            self.table = new_node
         self.load += 1
-
-        
-
-
 
     def delete(self, key):
         """
@@ -135,20 +129,39 @@ class HashTable:
         Implement this.
         """
         # Your code here
+        # hashed_string = self.hash_index(key)
+        # if self.table == None:
+        #     print("warning no key!!")
+
+        # else:
+
+        #    self.table[hashed_string] = None
+
+        #    self.load -= 1
         hashed_string = self.hash_index(key)
-        if self.table == None:
-            print("warning no key!!")
+        removed = False
+
+        if self.table[hashed_string].key == key:
+            self.table[hashed_string] = self.table[hashed_string].next
+            self.load -= 1
+            removed = True
 
         else:
-           
-           self.table[hashed_string] = None
+            curr = self.table[hashed_string]
+            prev_node = None
 
-           self.load -= 1
-     
-            
+            while curr is not None:
+                if curr.key == key:
+                    prev_node.next = curr.next
+                    self.load -= 1
+                    removed = True
 
+                    return
 
-
+                prev_node = curr
+                curr = curr.next
+            if removed == False:
+                print("warning no key!!")
 
     def get(self, key):
         """
@@ -159,13 +172,21 @@ class HashTable:
         Implement this.
         """
         # Your code here
+
+        # hashed_string = self.hash_index(key)
+
+        # value = self.table[hashed_string]
+
+        # return value if value else None
         hashed_string = self.hash_index(key)
-        
-        value = self.table[hashed_string]
+        current_node = self.table[hashed_string]
 
-        return value if value else None
-        
+        while current_node is not None:
+            if current_node.key == key:
+                return current_node.value
+            current_node = current_node.next
 
+        return None
 
     def resize(self, new_capacity):
         """
@@ -175,7 +196,29 @@ class HashTable:
         Implement this.
         """
         # Your code here
-        # dont do yet 
+        # save your old storage in a new variable
+        # make a new storage , with new capacity
+        # then loop over your old starage
+        #  for each index, if its not not
+        # iterate through the linked list
+        # for each node , call put for key and value
+        # that way, its rehashed and inserted to new storage
+        # self.load remain the same
+        # self.capacity
+        #
+        new_hashtable = HashTable(new_capacity)
+
+        for i in range(self.capacity):
+            if self.table[i] is not None:
+                entry = self.table[i]
+                while entry:
+                    new_hashtable.put(entry.key, entry.value)
+                    entry = entry.next
+
+        self.table = new_hashtable.table
+        self.capacity = new_capacity
+        self.load = new_hashtable.load
+
 
 hash_table = HashTable(100)
 
