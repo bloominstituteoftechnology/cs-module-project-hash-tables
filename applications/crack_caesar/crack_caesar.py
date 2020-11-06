fad3d5011b47c14d524a9d0e6afe43227864b8f1
CarlosETurcios <turcios50@gmail.com>
2020-11-05 19:40:24 -0800

    started applictions

--- applications/crack_caesar/crack_caesar.py
+++ applications/crack_caesar/crack_caesar.py
@@ -3,3 +3,22 @@
 
 
 # Your code here
+# U -  make a table with letter frequency in it ?
+# use the table to iderate through the ciphertext.text
+# the most fequent letter in the ciphertext will == the most common letter in the alphabel
+# to create a new table of common code .
+
+# p
+# E
+# R
+from csv import reader
+with open("ciphertext.txt") as file:
+    csv_reader = reader(file)
+    for letter in csv_reader:
+        print(letter)
+
+text_letter = letter
+
+
+def frequencyLetter():
+    pass
