5cc04f402cac88b9db5ea8aa01a0a5c3e2ffe686
Jose <pjose14.jp38@yahoo.com>
2020-11-05 20:51:54 -0600

    day 3

--- applications/expensive_seq/expensive_seq.py
+++ applications/expensive_seq/expensive_seq.py
@@ -6,11 +6,11 @@ def expensive_seq(x, y, z):
     key = (x, y, z)
     if store.get(key) is not None:
         return store[key]
-    
+     
     if x <= 0:
         return y + z
     
-    
+     
     store[key] = expensive_seq(
         x-1, y+1, z) + expensive_seq(x-2, y+2, z*2) + expensive_seq(x-3, y+3, z*3)
     
